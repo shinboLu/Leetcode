c2ecb9e331c73dc69c5a7116b05d6762695e0666
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-05 10:21:14 -0800

    Time: 333 ms (27.22%), Space: 24.2 MB (24.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,29 +1,40 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        DIR = [[1,0], [0,1], [-1, 0], [0,-1]]
         nrow = len(grid)
         ncol = len(grid[0])
-        islands = 0
-        DIR = [[1,0], [0,1], [0,-1], [-1, 0]]
+        visited = set()
+
+        def getNeighbors(x,y):
+            neighbor = []
+            for dx, dy in DIR:
+                new_x, new_y = dx + x, dy + y
+                if 0 <= new_x < nrow and 0 <= new_y < ncol:
+                    neighbor.append([new_x, new_y])
+            return neighbor
+
         def bfs(x, y):
-            queue = collections.deque([[x,y]])
-            grid[x][y] = '0'
-            while queue: 
-                x,y = queue.popleft()
-                for dirX, dirY in DIR:
-                    dx, dy = dirX + x, dirY + y
-                    if not is_valid(dx, dy):
-                        continue
-                    grid[dx][dy] = '0'
-                    queue.append([dx, dy])
+            queue = collections.deque()
+            queue.append((x,y))
+            visited.add((x,y))
+            islands = []
+            
+            while len(queue) > 0:
+                n = len(queue)
+                for _ in range(n):
+                    r,c = queue.popleft()
+                    for neighbor_x, neighbor_y in getNeighbors(r,c):
+                        if (neighbor_x, neighbor_y) not in visited and grid[neighbor_x][neighbor_y] == '1':
+                            visited.add((neighbor_x, neighbor_y))
+                            queue.append((neighbor_x, neighbor_y))
 
-        def is_valid(x, y):
-            return 0 <= x < nrow and 0 <= y < ncol and grid[x][y] == '1'
+        islands = 0
+        for row in range(nrow):
+            for col in range(ncol):
+                if grid[row][col] == '1' and (row,col) not in visited:
+                    islands += 1
+                    bfs(row, col)
+                    
+        return islands
 
-        for i in range(nrow):
-            for j in range(ncol):
-                if grid[i][j] == '1':
-                    ##start bfs()
-                    bfs(i,j)
-                    islands+=1
-        return islands 
-        
+            
