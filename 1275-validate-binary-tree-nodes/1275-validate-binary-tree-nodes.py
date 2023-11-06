9d30fb1a8a13bb824d72723d35a5293b111df5fe
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-05 22:18:45 -0800

    Time: 284 ms (34.92%), Space: 18.9 MB (66.08%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
@@ -1,8 +1,7 @@
 class unionFind:
     def __init__(self, size) -> None:
         self.root = [x for x in range(size)]
-        self.rank = [1] * size
-        self.ncomponent = size
+        self.ncomponents = size
 
     def find(self, x):
         if x == self.root[x]:
@@ -10,32 +9,24 @@ class unionFind:
         self.root[x] = self.find(self.root[x])
         return self.root[x]
 
-
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
         if root_x == root_y or root_y != y:
             return False
-        self.ncomponent -= 1
-        # if root_x != root_y:
-        #     if self.rank[root_x] > self.rank[root_y]:
-        #         self.rank[root_y] = root_x
-        #     elif  self.rank[root_x] < self.rank[root_y]:
-        #         self.rank[root_x] = root_y
-        #     else:
-                
+        self.ncomponents -= 1
         self.root[root_y] = root_x
-        self.rank[root_x] += 1
         return True
     
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
         uf = unionFind(n)
-        for node in range(n):
-            for child in [leftChild[node], rightChild[node]]:
-                print(child)
-                if child == -1:
+        graph = list(zip(leftChild, rightChild))
+        for row, rows in enumerate(graph):
+            for col, cols in enumerate(rows):
+                if cols == -1:
                     continue
-                if not uf.union(node, child):
+                if not uf.union(row, cols):
                     return False
-        return uf.ncomponent == 1
+
+        return uf.ncomponents == 1
