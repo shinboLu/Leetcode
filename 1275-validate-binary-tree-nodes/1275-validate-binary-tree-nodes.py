55a02f9348a423b370548ca69f1297d6d2ba041b
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-06 11:25:31 -0800

    Time: 309 ms (18.64%), Space: 18.5 MB (73.3%) - LeetHub

--- 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
+++ 1275-validate-binary-tree-nodes/1275-validate-binary-tree-nodes.py
@@ -1,32 +1,44 @@
 class unionFind:
     def __init__(self, size) -> None:
         self.root = [x for x in range(size)]
-        self.ncomponents = size
+        self.rank = [1] * size
+        self.component = size
 
     def find(self, x):
-        if x == self.root[x]:
+        if self.root[x] == x:
             return x
         self.root[x] = self.find(self.root[x])
         return self.root[x]
 
-    def union(self, x, y):
+    def union(self,x, y):
         root_x = self.find(x)
         root_y = self.find(y)
-        if root_x == root_y or root_y != y:
+        if root_x == root_y or root_y != y :
             return False
-        self.ncomponents -= 1
+        
+        # if root_x != root_y:
+        #     if self.rank[root_x] > self.rank[root_y]:
+        #         self.root[root_y] = root_x
+        #     elif self.rank[root_x] < self.rank[root_y]:
+        #         self.root[root_x] = root_y
+        #     else: 
         self.root[root_y] = root_x
+        self.rank[root_x] += 1
+        self.component -=1 
         return True
-    
+
 class Solution:
     def validateBinaryTreeNodes(self, n: int, leftChild: List[int], rightChild: List[int]) -> bool:
-        uf = unionFind(n)
         graph = list(zip(leftChild, rightChild))
-        for row, rows in enumerate(graph):
-            for col, cols in enumerate(rows):
-                if cols == -1:
-                    continue
-                if not uf.union(row, cols):
-                    return False
+        uf = unionFind(n)
 
-        return uf.ncomponents == 1
+        for root in range(n):
+            for child in [leftChild[root], rightChild[root]]:
+                if child == -1:
+                    continue
+                
+                if not uf.union(root, child):
+                    return False
+        return uf.component == 1 
+                
+                
