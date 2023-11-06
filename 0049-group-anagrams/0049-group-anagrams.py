145a8e86038c39e403b5d9540499e39bc738e5e0
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-06 14:07:54 -0800

    Time: 93 ms (82.87%), Space: 20.2 MB (48.06%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,8 +1,12 @@
+from string import ascii_lowercase
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
-        return ans.values()
-        
-        
+        sorted_dic = collections.defaultdict(list)
+
+        for string in strs:
+            sorted_string = ''.join(sorted(string))
+            sorted_dic[sorted_string].append(string)
+
+        return sorted_dic.values()
+
