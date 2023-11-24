b79aca6b9178c04f7d3a2d1cd18561ac0e7ac5ae
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-23 22:37:26 -0800

    Time: 1884 ms (9.50%) | Memory: 27.3 MB (65.55%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.py
+++ 310-minimum-height-trees/minimum-height-trees.py
@@ -1,32 +1,35 @@
 class Solution:
     def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
         if n <= 2:
-            return [i for i in range(n)]
+            return [x for x in range(n)]
 
         adj_list = collections.defaultdict(list)
-        indegree = [0] * n
 
         for u, v in edges:
             adj_list[u].append(v)
             adj_list[v].append(u)
-        print(adj_list)
+
+        ## starting from the leaves
         leaves = collections.deque()
         for i in range(n):
             if len(adj_list[i]) == 1:
                 leaves.append(i)
-        remaining_node = n
-        while remaining_node > 2: 
-            remaining_node -= len(leaves)
+        ## "peeling the adj_list, untill the centroids are left"
+        while n > 2:
+            ## update the remaining nodes
+            n -= len(leaves)
             next_level_leaves = []
+
             while leaves:
-                leaf = leaves.pop()
-                neighbor = adj_list[leaf].pop()
-                adj_list[neighbor].remove(leaf)
-                if len(adj_list[neighbor]) == 1:
-                    next_level_leaves.append(neighbor)
+                curr_leaves = leaves.pop()
+                ## get and remove the leaf connected to the current level of leaves
+                neigh = adj_list[curr_leaves].pop()
+                ## remove the current node from the connected leaf
+                adj_list[neigh].remove(curr_leaves)
+                if len(adj_list[neigh]) == 1:
+                    next_level_leaves.append(neigh)
+                
             leaves = next_level_leaves
 
         return leaves
 
-
-        
