b82e31e2603369eed43e4c41368011688e3f701a
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-25 16:35:10 -0800

    Time: 1068 ms (74.07%) | Memory: 17.1 MB (48.15%) - LeetSync

--- 2612-minimum-cost-to-buy-apples/minimum-cost-to-buy-apples.py
+++ 2612-minimum-cost-to-buy-apples/minimum-cost-to-buy-apples.py
@@ -1,27 +1,31 @@
+from heapq import heappop, heappush, heapify
 class Solution:
     def minCost(self, n: int, roads: List[List[int]], appleCost: List[int], k: int) -> List[int]:
         graph = collections.defaultdict(list)
-        for u, v, cost in roads:
-            graph[u-1].append((v-1, cost))
-            graph[v-1].append((u-1,cost))
+        
+        for u, v, w in roads:
+            graph[u-1].append((v-1, w))
+            graph[v -1].append((u-1, w))
 
-        cost_list = [float('inf')] * n
-
+        ## store the min cost to get an apple from each vertex
+        res = [float('inf')] * n
+        ## start with each vertex
         for i in range(n):
-            dist = [float('inf')] * n
-            dist[i] = 0 
+            distance = [float('inf')] * n 
+            ## each vertex is 0 because starting from itself
+            distance[i] = 0
             pq = [(0,i)]
             while pq:
-                d, u = heapq.heappop(pq)
-                cost_list[i] = min(cost_list[i], appleCost[u] + (k + 1) * d)
-                for v, w in graph[u]:
-                    temp_d = d + w
-                    if temp_d < dist[v]:
-                        dist[v] = temp_d
-                        heapq.heappush(pq, (temp_d, v))
+                w, cur_pos = heapq.heappop(pq)
+                res[i] = min(res[i], appleCost[cur_pos] + (k+1) * w)
+                for next_pos, next_w in graph[cur_pos]:
+                    next_vertex_cost = w + next_w
+                    if next_vertex_cost < distance[next_pos]:
+                        distance[next_pos] = next_vertex_cost
+                        heappush(pq, (next_vertex_cost, next_pos))
 
-        return cost_list
+        return res
 
-        
+
 
         
