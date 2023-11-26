aa237eb539b53fd81fbbfe2f7c13a9fbedfdbe50
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-11-25 17:36:47 -0800

    Time: 967 ms (97.85%) | Memory: 30.3 MB (66.95%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.py
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.py
@@ -1,16 +1,9 @@
 class Solution:
     def edgeScore(self, edges: List[int]) -> int:
-        graph = collections.defaultdict(list)
-        res = [0] * len(edges)
-        for idx, val in enumerate(edges):
-            graph[val].append(idx)
-
-        for k ,v in graph.items():
-            for node in v:
-                res[k] += node
-
-        return res.index(max(res))
-        
+        array = [0] * len(edges)
+        for num, index in enumerate(edges):
+            array[index] += num
+        return array.index(max(array))
 
 
         
