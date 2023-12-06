b9563c8fa0464a32392008d733671547226a084b
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-05 17:13:13 -0800

    Time: 43 ms (43.20%) | Memory: 16.5 MB (9.47%) - LeetSync

--- 1191-synonymous-sentences/synonymous-sentences.py
+++ 1191-synonymous-sentences/synonymous-sentences.py
@@ -1,26 +1,62 @@
+from collections import defaultdict
 class Solution:
     def generateSentences(self, synonyms: List[List[str]], text: str) -> List[str]:
-        G = defaultdict(set)
-        for x,y in synonyms:
-            G[x].add(y)
-            G[y].add(x)
+        graph = defaultdict(list)
+        for n1, n2 in synonyms:
+            graph[n1].append(n2)
+            graph[n2].append(n1)
         
-        def dfs(w,ans):
-            ans.add(w)
-            for x in G[w]:
-                if x not in ans: dfs(x,ans)
+        word_to_syn = defaultdict(set)
+
+        def dfs(word):
+            if word in syns:
+                return
+            syns.add(word)
+            for edge in graph[word]:
+                dfs(edge)
+        
+        for key in graph.keys():
+            syns = set()
+            dfs(key)
+            word_to_syn[key] = syns
+        
+        sentence = text.split()
+        ans = []
+
+        def backtrack(i):
+            if i == len(sentence):
+                ans.append(' '.join(sentence))
+                return
+            curr_word = sentence[i]
+            if curr_word in word_to_syn:
+                for syn in word_to_syn[curr_word]:
+                    sentence[i] = syn
+                    backtrack(i + 1)
+                    sentence[i] = curr_word
+            else:
+                backtrack(i + 1)
+        
+        backtrack(0)
+        ans.sort()
+        return ans
+
+
+                        
+
+        
+        
+
+
+
+
+
+        
+                        
                     
-        X = text.split()
-        out = []
-        for w in X:
-            out1 = []
-            if w in G:
-                wset = set()
-                dfs(w, wset)
-                for w1 in wset:
-                    if out: out1.extend([x+" "+w1 for x in out])
-                    else: out1.append(w1)
-            else: out1 = [x+" "+w for x in out] if out else [w]
-            out = out1
-        return sorted(out)
+
+            
+                        
+
+            
+
         
