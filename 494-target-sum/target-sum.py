59b7df212ac61ad988de227d8830c2d42b898671
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-10 14:41:09 -0800

    Time: 245 ms (62.40%) | Memory: 37.7 MB (42.32%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -6,18 +6,15 @@ class Solution:
             if idx == len(nums):
                 if total == target:
                     return 1
-                else:
+                else: 
                     return 0
 
-            
             key = (idx, total)
-
             if key in memo:
                 return memo[key]
 
-            memo[key] = (backtracking(idx+1, total + nums[idx]) + backtracking(idx+1, total-nums[idx]))
+            memo[key] = (backtracking(idx+1, total+nums[idx]) + backtracking(idx+1, total - nums[idx]))
 
             return memo[key]
-
 
         return backtracking(0,0)
