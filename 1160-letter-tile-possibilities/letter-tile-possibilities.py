6d2bca90cda13ed481d02aa40be19f0ae6510fc8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-10 15:43:14 -0800

    Time: 158 ms (15.47%) | Memory: 25.9 MB (6.65%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,23 +1,39 @@
 class Solution:
-    def numTilePossibilities(self, tiles: str) -> int:
-        record = [0] * 26
+    def numTilePossibilities(self, tiles: str) -> int: 
+        res = set()
 
-        for tile in tiles:
-            record[ord(tile) - ord('A')] += 1
+        def backtracking(t, curr , index):
+            if index == len(curr):
+                res.add(curr)
+                return 
 
-        print(record)
+            for i in range(len(t)):
+                backtracking(t[:i] + t[i+1:], curr+t[i], index)
+
+        for i in range(1, len(tiles)+1):
+            backtracking(tiles, '', i)
+        return len(res)
+         
+
+
+        # record = [0] * 26
+
+        # for tile in tiles:
+        #     record[ord(tile) - ord('A')] += 1
+
+        # print(record)
         
-        def dfs(record):
-            s = 0 
-            for i in range(26):
-                if not record[i]:
-                    continue
-                record[i] -= 1
-                s += dfs(record) + 1
-                record[i] += 1
-            return s
+        # def dfs(record):
+        #     s = 0 
+        #     for i in range(26):
+        #         if not record[i]:
+        #             continue
+        #         record[i] -= 1
+        #         s += dfs(record) + 1
+        #         record[i] += 1
+        #     return s
 
-        return dfs(record)
+        # return dfs(record)
 
 
 
