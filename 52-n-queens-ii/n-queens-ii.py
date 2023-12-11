92035c8e7f62478d3ff5c55d5b42aca9ea431d6c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-10 21:52:37 -0800

    Time: 53 ms (55.56%) | Memory: 16.3 MB (67.18%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.py
+++ 52-n-queens-ii/n-queens-ii.py
@@ -1,29 +1,56 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        
-        def backtracking(row, diagonals, anti_diagonals, cols):
+
+        def backtracking(diagonals, anti_diagonals, row, cols):
             if row == n:
                 return 1
 
-            solutions = 0
+            
+            res = 0
 
             for col in range(n):
                 curr_diagonal = row - col
-                curr_anti_diagonal = row + col 
+                curr_anti_diagonal = row + col
+
                 if col in cols or curr_diagonal in diagonals or curr_anti_diagonal in anti_diagonals:
                     continue
-
+                
                 cols.add(col)
                 diagonals.add(curr_diagonal)
                 anti_diagonals.add(curr_anti_diagonal)
 
-                solutions += backtracking(row+1, diagonals, anti_diagonals, cols)
+                res += backtracking(diagonals, anti_diagonals, row+1, cols)
 
                 cols.remove(col)
                 diagonals.remove(curr_diagonal)
                 anti_diagonals.remove(curr_anti_diagonal)
-            return solutions
+            return res 
+        return backtracking(set(), set(), 0, set())
+
         
-        return backtracking(0, set(), set(), set())
+        # def backtracking(row, diagonals, anti_diagonals, cols):
+        #     if row == n:
+        #         return 1
+
+        #     solutions = 0
+
+        #     for col in range(n):
+        #         curr_diagonal = row - col
+        #         curr_anti_diagonal = row + col 
+        #         if col in cols or curr_diagonal in diagonals or curr_anti_diagonal in anti_diagonals:
+        #             continue
+
+        #         cols.add(col)
+        #         diagonals.add(curr_diagonal)
+        #         anti_diagonals.add(curr_anti_diagonal)
+
+        #         solutions += backtracking(row+1, diagonals, anti_diagonals, cols)
+
+        #         cols.remove(col)
+        #         diagonals.remove(curr_diagonal)
+        #         anti_diagonals.remove(curr_anti_diagonal)
+        #     return solutions
+        
+        # return backtracking(0, set(), set(), set())
 
 
