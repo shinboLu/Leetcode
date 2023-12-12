1c40240c5952d8946d93dbf5da9c016ab7e7186f
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-11 23:05:44 -0800

    Time: 959 ms (29.20%) | Memory: 64.8 MB (53.64%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -2,29 +2,22 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
 
-        ## store all possible and correct combination 
-        def bt_update_res(index, path):
-          
-            if index == n + 1:
+        def find_unique_combination(index, path):
+            if index == n:
                 if len(path) == k:
                     res.append(path.copy())
-                return 
-            if len(path) == k:
-               
-                res.append(path.copy())
-             
-                return 
+                    return
 
- 
+            if len(path) == k:
+                res.append(path.copy())
+                return
+
             for i in range(index, n+1):
                 path.append(i)
-                bt_update_res(i + 1, path)
-          
+                find_unique_combination(i+1, path)
                 path.pop()
 
-        bt_update_res(1, [])
-
-
+        find_unique_combination(1, [])
         return res 
-                
+
             
