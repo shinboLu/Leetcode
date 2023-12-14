0b5055747dbd0bada80c46f695564956a49c4150
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-13 20:00:58 -0800

    Time: 183 ms (26.34%) | Memory: 16.5 MB (11.78%) - LeetSync

---
+++ 567-permutation-in-string/permutation-in-string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter, w = collections.Counter(s1), len(s1)   
+
+        for i in range(len(s2)):
+            if s2[i] in counter: 
+                counter[s2[i]] -= 1
+            if i >= w and s2[i-w] in counter: 
+                counter[s2[i-w]] += 1
+
+            if all([counter[i] == 0 for i in counter]): # see optimized code below
+                return True
+
+        return False
