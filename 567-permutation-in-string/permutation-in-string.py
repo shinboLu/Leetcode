52ca3e698cd1062a9c15272628499a2960cd32ca
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-13 20:07:43 -0800

    Time: 207 ms (25.16%) | Memory: 16.5 MB (39.03%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -6,9 +6,10 @@ class Solution:
             if s2[i] in counter: 
                 counter[s2[i]] -= 1
             if i >= w and s2[i-w] in counter: 
+                print(s2[i-w])
                 counter[s2[i-w]] += 1
 
-            if all([counter[i] == 0 for i in counter]): # see optimized code below
+            if all([counter[i] == 0 for i in counter]): 
                 return True
 
         return False
