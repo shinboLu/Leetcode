2157c631c142d4dd961dc29ada6c4e39faf82532
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-13 18:37:23 -0800

    Time: 41 ms (49.18%) | Memory: 16.6 MB (18.45%) - LeetSync

---
+++ 91-decode-ways/decode-ways.py
@@ -0,0 +1,40 @@
+from string import ascii_uppercase
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        @lru_cache(maxsize=None)
+        def backtracking(index):
+            if index == len(s):
+                return 1
+            
+            if s[index] == '0':
+                return 0
+
+            if index == len(s)-1:
+                return 1
+            
+            res = backtracking(index+1)
+
+            if int(s[index:index+2]) <= 26:
+                res += backtracking(index+2)
+
+            return res
+
+
+        return backtracking(0)
+            
+
+
+            
+            
+
+
+        backtracking(1,[])
+        print(res)
+        return len(res)
+
+
+
+
+
+
+        
