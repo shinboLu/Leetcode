d46b888487d7f3d9c1db79e892058d77bb59e1c5
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-15 22:16:07 -0800

    Time: 1237 ms (33.51%) | Memory: 16.3 MB (69.40%) - LeetSync

--- 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/fair-distribution-of-cookies.py
@@ -1,32 +1,36 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        cur = [0] * k
+        ## plates[i]: for child i, he has # of cookies
+        plates = [0] * k
+
         n = len(cookies)
 
-        def dfs(i, zero_count):
-            # If there are not enough cookies remaining, return `float('inf')` 
-            # as it leads to an invalid distribution.
-            if n - i < zero_count:
+        def place_cookies(cookie_index, empty_plates):
+            ## trimming: if there are less cookies than k, terminate
+
+            if n - cookie_index < empty_plates:
                 return float('inf')
-            
-            # After distributing all cookies, return the unfairness of this
-            # distribution.
-            if i == n:
-                return max(cur)
-            
-            # Try to distribute the i-th cookie to each child, and update answer
-            # as the minimum unfairness in these distributions.
-            answer = float('inf')
-            for j in range(k):
-                zero_count -= int(cur[j] == 0)
-                cur[j] += cookies[i]
+
+            ## base case 2: if distributed all cookies, return the max 
+            ##      total cookies obtained by a single child in the distribution
+            if cookie_index == n:
+                return max(plates)
+
+            res = float('inf')
+            ## maintainance
+
+            for i in range(k):
+                ## if the ith kid has cookie, reduce the number of total empty plates by 1
+                empty_plates -= int(plates[i] == 0)
+
+                plates[i] += cookies[cookie_index]
+                res = min(res, place_cookies(cookie_index+1, empty_plates))
+                plates[i] -= cookies[cookie_index]
+                empty_plates += int(plates[i] == 0)
                 
-                # Recursively distribute the next cookie.
-                answer = min(answer, dfs(i + 1, zero_count))
-                
-                cur[j] -= cookies[i]
-                zero_count += int(cur[j] == 0)
-            
-            return answer
+
+            return res 
+
+        return place_cookies(0, k)
         
-        return dfs(0, k)
+
