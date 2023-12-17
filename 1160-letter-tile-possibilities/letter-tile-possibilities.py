c43ee02375f15891f81f37f2c04807276bc2732a
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-17 14:28:59 -0800

    Time: 141 ms (22.15%) | Memory: 24.8 MB (22.64%) - LeetSync

--- 1160-letter-tile-possibilities/letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/letter-tile-possibilities.py
@@ -1,41 +1,22 @@
 class Solution:
-    def numTilePossibilities(self, tiles: str) -> int: 
+    def numTilePossibilities(self, tiles: str) -> int:
         res = set()
+        visited = [False] * len(tiles)
+        def backtracking(combs):
+            res.add(''.join(combs))
 
-        def backtracking(t, curr , index):
-            if index == len(curr):
-                res.add(curr)
-                return 
+            for i in range(len(tiles)):
+                if visited[i]:
+                    continue
+                visited[i] = True
+                combs.append(tiles[i])
+                backtracking(combs)
+                combs.pop()
+                visited[i] = False
+        backtracking([])
+        print(res )
+        return len(res)-1        
 
-            for i in range(len(t)):
-                backtracking(t[:i] + t[i+1:], curr+t[i], index)
+                
 
-        for i in range(1, len(tiles)+1):
-            backtracking(tiles, '', i)
-        return len(res)
-         
-
-
-        # record = [0] * 26
-
-        # for tile in tiles:
-        #     record[ord(tile) - ord('A')] += 1
-
-        # print(record)
         
-        # def dfs(record):
-        #     s = 0 
-        #     for i in range(26):
-        #         if not record[i]:
-        #             continue
-        #         record[i] -= 1
-        #         s += dfs(record) + 1
-        #         record[i] += 1
-        #     return s
-
-        # return dfs(record)
-
-
-
-
-            
