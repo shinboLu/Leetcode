00681e2a800fbbcbd387b7b3b615d9556809b266
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-25 15:38:23 -0800

    Time: 44 ms (26.50%) | Memory: 17.3 MB (5.05%) - LeetSync

--- 666-path-sum-iv/path-sum-iv.py
+++ 666-path-sum-iv/path-sum-iv.py
@@ -5,42 +5,41 @@ class TreeNode:
         self.right = right
 class Solution:
     def pathSum(self, nums: List[int]) -> int:
-        # nums.sort(key = lambda x: x // 100)
-        # print(nums )
         tree = {}
+        
+        for num in nums:
+            depth = int(str(num)[0])
+            pos = int(str(num)[1])
+            val = int(str(num)[2])
 
-        for num in nums: 
-            depth, pos, val = num // 100, num // 10 % 10, num % 10
             node = TreeNode(val)
 
             tree[(depth, pos)] = node
 
             if depth == 1:
                 root = node
-            else: 
-                parent = tree[(depth-1), (pos +1)//2]
+
+            else:
+                parent = tree[(depth-1, (pos+1)//2)]
 
                 if pos % 2 == 1:
                     parent.left = node
                 else:
                     parent.right = node
 
-        res = 0
+        res=0
 
         stack = [(root, root.val)]
 
-        while stack: 
+        while stack:
             node, val = stack.pop()
             if node:
                 if not node.left and not node.right:
                     res += val
-                
                 if node.left:
                     stack.append((node.left, val + node.left.val))
-                
                 if node.right:
                     stack.append((node.right, val + node.right.val))
-
         return res 
 
-        return 0
+
