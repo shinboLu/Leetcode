992a478679f873563a34a8f2ae7a78da329e5499
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-26 20:38:29 -0800

    Time: 59 ms (73.27%) | Memory: 18 MB (5.03%) - LeetSync

--- 588-design-in-memory-file-system/design-in-memory-file-system.py
+++ 588-design-in-memory-file-system/design-in-memory-file-system.py
@@ -12,41 +12,79 @@ class FileSystem:
 
     def ls(self, path: str) -> List[str]:
         res = []
-        path = path.split('/')[1:]
+        paths = path.split('/')[1:]
         cur = self.root
-        if path[0] != '':
-            for p in path:
+        if paths[0] != '':
+            for p in paths:
                 cur = cur.child[p]
         if cur.isFile:
             return [cur.name]
         for ch in cur.child:
             res.append(ch)
+
         return sorted(res)
+        # res = []
+        # path = path.split('/')[1:]
+        # cur = self.root
+        # if path[0] != '':
+        #     for p in path:
+        #         cur = cur.child[p]
+        # if cur.isFile:
+        #     return [cur.name]
+        # for ch in cur.child:
+        #     res.append(ch)
+        # return sorted(res)
 
     def mkdir(self, path: str) -> None:
         cur = self.root
         paths = path.split('/')[1:]
+
         for p in paths:
             cur = cur.child[p]
             cur.name = p
 
+        # cur = self.root
+        # paths = path.split('/')[1:]
+        # for p in paths:
+        #     cur = cur.child[p]
+        #     cur.name = p
+
     def addContentToFile(self, filePath: str, content: str) -> None:
         cur = self.root
-        filePath = filePath.split('/')[1:]
-        for p in filePath:
+        paths = filePath.split('/')[1:]
+
+        for p in paths:
             cur = cur.child[p]
             cur.name = p
+
         cur.isFile = True
         cur.content += content
+
+        # cur = self.root
+        # filePath = filePath.split('/')[1:]
+        # for p in filePath:
+        #     cur = cur.child[p]
+        #     cur.name = p
+        # cur.isFile = True
+        # cur.content += content
         
 
     def readContentFromFile(self, filePath: str) -> str:
         cur = self.root
-        filePath = filePath.split('/')[1:]
-        for p in filePath:
+        paths = filePath.split('/')[1:]
+
+        for p in paths:
             cur = cur.child[p]
         if cur.isFile:
             return cur.content
+
+
+        # cur = self.root
+        # filePath = filePath.split('/')[1:]
+        # for p in filePath:
+        #     cur = cur.child[p]
+        # if cur.isFile:
+        #     return cur.content
         
 
 
