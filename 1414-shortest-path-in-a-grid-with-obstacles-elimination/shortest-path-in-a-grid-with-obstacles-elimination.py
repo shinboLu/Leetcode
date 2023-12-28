cffe3fb763ef5a60094b21f8b73ffc045ca379c2
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-27 22:21:28 -0800

    Time: 1094 ms (5.04%) | Memory: 25.4 MB (7.94%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -12,21 +12,23 @@ class Solution:
 
         while queue:
             cur_x, cur_y, k_left, steps = queue.popleft()
-            if (cur_x,cur_y, k_left) in visited or k_left < 0:
+            if (cur_x, cur_y, k_left) in visited or k_left < 0:
                 continue
+
             if (cur_x, cur_y) == (nrow-1, ncol-1):
                 return steps
 
             visited.add((cur_x, cur_y, k_left))
 
             if grid[cur_x][cur_y] == 1:
-                k_left -= 1
+                k_left -= 1 
+            
 
-            for dx, dy in DIR: 
-                nx, ny = cur_x + dx, cur_y + dy  
-                if 0 <= nx < nrow and 0 <= ny < ncol:
-                    queue.append((nx, ny, k_left, steps + 1))
-
+            for dx, dy in DIR:
+                nx, ny = dx+cur_x, dy + cur_y
+                if 0<= nx < nrow and 0 <= ny < ncol:
+                    queue.append((nx, ny, k_left, steps+1))
+                
         return -1
         
 
