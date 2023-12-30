28d72af5f716a1395305b2c2da155cc867d62f3a
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-29 18:34:53 -0800

    Time: 1075 ms (9.88%) | Memory: 78.8 MB (9.82%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,25 +1,52 @@
+class ListNode:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.next = None
+        self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = collections.OrderedDict()
+        self.head = ListNode(-1, -1)
+        self.tail = ListNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.cache = {}
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.cache.move_to_end(key)
-            return self.cache[key]
-        return -1
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.add(node) 
+        return node.val
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
+            old_node = self.cache[key]
+            self.remove(old_node)
+        
+        node = ListNode(key, value)
+        self.cache[key] = node
+        self.add(node)
 
         if len(self.cache) > self.capacity:
-            self.cache.popitem(last = False)
+            node_to_delete = self.head.next
+            self.remove(node_to_delete)
+            del self.cache[node_to_delete.key]
 
-        
+    def add(self, node):
+        prev_end = self.tail.prev
+        prev_end.next = node
+        node.prev = prev_end
+        node.next = self.tail
+        self.tail.prev = node
 
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
