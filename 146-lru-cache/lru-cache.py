052bd86c04e4dbd6e012c11081161c88b49b39b8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-30 18:05:47 -0800

    Time: 839 ms (12.19%) | Memory: 79.3 MB (6.01%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,8 +2,9 @@ class ListNode:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.next = None
         self.prev = None
+        self.next = None
+
 
 class LRUCache:
 
@@ -12,17 +13,29 @@ class LRUCache:
         self.head = ListNode(-1, -1)
         self.tail = ListNode(-1, -1)
         self.head.next = self.tail
-        self.tail.prev = self.head
+        self.tail.prev = self.head 
         self.cache = {}
+
+    def add(self, node):
+        prev_end = self.tail.prev 
+        prev_end.next = node
+        node.prev = prev_end
+        node.next = self.tail
+        self.tail.prev = node
+
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
 
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
+
         node = self.cache[key]
         self.remove(node)
-        self.add(node) 
-        return node.val
-
+        self.add(node)
+        return node.val 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             old_node = self.cache[key]
@@ -33,20 +46,11 @@ class LRUCache:
         self.add(node)
 
         if len(self.cache) > self.capacity:
-            node_to_delete = self.head.next
-            self.remove(node_to_delete)
-            del self.cache[node_to_delete.key]
+            remove_node = self.head.next
+            self.remove(remove_node)
+            del self.cache[remove_node.key]
+            
 
-    def add(self, node):
-        prev_end = self.tail.prev
-        prev_end.next = node
-        node.prev = prev_end
-        node.next = self.tail
-        self.tail.prev = node
-
-    def remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
