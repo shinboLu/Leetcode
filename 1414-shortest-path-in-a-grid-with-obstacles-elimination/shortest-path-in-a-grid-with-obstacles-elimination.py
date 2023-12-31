d995e1967bec370ad059244246e5450affd3ca7e
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2023-12-30 20:21:02 -0800

    Time: 501 ms (48.17%) | Memory: 25.1 MB (9.69%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -5,30 +5,27 @@ class Solution:
         nrow = len(grid)
         ncol = len(grid[0])
 
-        visited = set()
-
-        queue = deque()
-        queue.append((0, 0, k, 0))
+        visited = set() 
+        queue = collections.deque()
+        queue.append(((0,0, k, 0)))
 
         while queue:
-            cur_x, cur_y, k_left, steps = queue.popleft()
-            if (cur_x, cur_y, k_left) in visited or k_left < 0:
+            cur_x, cur_y, left, steps = queue.popleft() 
+            if (cur_x, cur_y, left) in visited or left < 0:
                 continue
+            if (cur_x, cur_y) == (nrow -1, ncol-1):
+                return steps 
 
-            if (cur_x, cur_y) == (nrow-1, ncol-1):
-                return steps
+            visited.add((cur_x, cur_y, left))
 
-            visited.add((cur_x, cur_y, k_left))
-
-            if grid[cur_x][cur_y] == 1:
-                k_left -= 1 
-            
+            if grid[cur_x][cur_y] == 1: 
+                left -= 1
 
             for dx, dy in DIR:
-                nx, ny = dx+cur_x, dy + cur_y
-                if 0<= nx < nrow and 0 <= ny < ncol:
-                    queue.append((nx, ny, k_left, steps+1))
-                
+                nx, ny = dx + cur_x, dy + cur_y
+                if 0 <= nx < nrow and 0 <= ny < ncol:
+                    queue.append((nx, ny, left, steps + 1))
+
         return -1
-        
+
 
