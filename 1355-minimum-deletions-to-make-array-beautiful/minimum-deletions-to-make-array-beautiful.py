eced06869af134b7442b6bcb96f23808eda273d4
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-05 23:06:27 -0800

    Time: 929 ms (100.00%) | Memory: 31.5 MB (30.19%) - LeetSync

--- 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.py
+++ 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.py
@@ -1,17 +1,18 @@
 class Solution:
     def minDeletion(self, nums: List[int]) -> int:
-        res = 0
         n = len(nums)
         isEven = True
-        for i in range(n):
+        res = 0
+
+        for i in range(n-1):
             if isEven:
-                if i % 2 == 0 and i != n -1 and nums[i] == nums[i+1]:
+                if i % 2 == 0  and nums[i] == nums[i+1]:
                     res += 1
                     isEven = False
 
             elif not isEven:
-                if i % 2 == 1 and i !=n -1 and nums[i] == nums[i+1]:
+                if i % 2 == 1 and nums[i] == nums[i+1]  :
                     res += 1
                     isEven = True
-        curLen = n - res 
-        return res if curLen % 2 == 0 else res + 1
+        cur_len = n - res
+        return res if cur_len % 2 ==0 else res + 1
