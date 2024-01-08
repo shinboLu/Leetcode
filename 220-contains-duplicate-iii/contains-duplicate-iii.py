79eaa9eec4346db629b9e1e02b8efda0955e6807
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-08 15:03:53 -0800

    Time: 836 ms (59.71%) | Memory: 46.1 MB (7.61%) - LeetSync

---
+++ 220-contains-duplicate-iii/contains-duplicate-iii.py
@@ -0,0 +1,23 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        if not nums or valueDiff < 0:
+            return False
+
+        min_val = min(nums)
+        # A lambda function generate buckey key given a value
+        bucket_key = lambda x: (x-min_val) // (valueDiff +1)
+        
+        d = collections.defaultdict(lambda: sys.maxsize)
+
+        for i, num in enumerate(nums):
+            num_bucket_key = bucket_key(num)
+            for nei in [d[num_bucket_key-1], d[num_bucket_key], d[num_bucket_key+1]]:
+                if abs(nei - num) <= valueDiff:
+                    return True
+            d[num_bucket_key] = num
+            if i >= indexDiff:
+                d.pop(bucket_key(nums[i-indexDiff]))
+        return False
+
+
+
