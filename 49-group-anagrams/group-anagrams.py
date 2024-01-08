7a9a35a5ee627dcb297ac8638f5650dc52f5f162
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-07 17:51:11 -0800

    Time: 96 ms (66.23%) | Memory: 20.3 MB (57.79%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.py
@@ -0,0 +1,21 @@
+from string import ascii_lowercase
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        if len(strs) <= 1:
+            return [strs]
+
+        counter = collections.defaultdict(list)
+
+        for s in strs:
+            sorted_s = ''.join(sorted(s))
+            print(sorted_s)
+            counter[sorted_s].append(s) 
+
+        res = []
+
+        for k, v in counter.items():
+            res.append(v)
+
+        return res 
