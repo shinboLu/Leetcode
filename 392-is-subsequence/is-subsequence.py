96953de7428275f1b896b9e75cd2c97d7a42c4cc
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-15 14:59:42 -0800

    Time: 68 ms (6.88%) | Memory: 18.4 MB (17.54%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        nrow = len(s)
+        ncol = len(t)
+
+        dp = [[0] * (ncol+1) for _ in range(nrow+1)]
+
+        for i in range(nrow-1, -1, -1):
+            for j in range(ncol-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = dp[i+1][j+1]+1
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+
+        print(dp[0][0], len(s))
+        if dp[0][0] == len(s):
+            return True
+        else:
+            return False
