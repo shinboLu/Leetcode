8f01b1419ab0889fd5569df939fb681b87f56a9a
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-15 21:56:51 -0800

    Time: 632 ms (70.00%) | Memory: 17.3 MB (97.61%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.py
@@ -1,14 +1,13 @@
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
         n = len(jobDifficulty)
-        # Initialize the min_diff matrix to record the minimum difficulty
-        # of the job schedule        
+
         min_diff = [[float('inf')] * n + [0] for i in range(d + 1)]
         for days_remaining in range(1, d + 1):
             for i in range(n - days_remaining + 1):
                 daily_max_job_diff = 0
                 for j in range(i + 1, n - days_remaining + 2):
-                    # Use daily_max_job_diff to record maximum job difficulty
+
                     daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j - 1])
                     min_diff[days_remaining][i] = min(min_diff[days_remaining][i], daily_max_job_diff + min_diff[days_remaining - 1][j])
         if min_diff[d][0] == float('inf'):
