93addde66428f47f47bcd517ecb3e20d5eb5c816
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-16 23:43:40 -0800

    Time: 686 ms (34.21%) | Memory: 18.4 MB (30.70%) - LeetSync

--- 3273-most-expensive-item-that-can-not-be-bought/most-expensive-item-that-can-not-be-bought.py
+++ 3273-most-expensive-item-that-can-not-be-bought/most-expensive-item-that-can-not-be-bought.py
@@ -2,23 +2,17 @@ class Solution:
     def mostExpensiveItem(self, primeOne: int, primeTwo: int) -> int:
         n = primeOne * primeTwo
         dp = [True] * (n+1) 
-        #dp[0] = 
         res = 0 
         for i in range(1,n+1):
             if i < primeOne or i < primeTwo:
-
                 if i % primeOne != 0 and i % primeTwo !=0 :
-
                     dp[i] = False
-
             else:
-
                 dp[i] = dp[i-primeOne] or dp[i-primeTwo]
 
         for idx, value in enumerate(dp):
             if value == False:
                 res = idx
-        # if len(dp) > 20:
-        #     print(dp[18], dp[15], dp[11])
+
 
         return res
