94f35416770f9976d67053a065cd61cd12c7666f
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-17 15:53:01 -0800

    Time: 4425 ms (9.45%) | Memory: 256.5 MB (5.05%) - LeetSync

--- 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
+++ 1761-count-sorted-vowel-strings/count-sorted-vowel-strings.py
@@ -1,13 +1,10 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
         vowels = ['a', 'e', 'i', 'o', 'u']
-        # m = len(vowels)
 
-        # dp = [[0] * (m+1) for _ in range(n+1)]
         res = []
         def backtrack(index, combs):
             if len(combs) == n:
-               # print(combs)
                 res.append(''.join(combs))
                 return 
 
@@ -15,7 +12,6 @@ class Solution:
 
                 if vowels[i-1] <= vowels[i]:
                     combs.append(vowels[i])
-                    #print(combs)
                     backtrack(i, combs)                
                     combs.pop()
 
