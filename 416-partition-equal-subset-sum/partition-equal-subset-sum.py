bba7009b886babc71c244004cd5789b885a9cd79
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-18 16:53:54 -0800

    Time: 1144 ms (41.61%) | Memory: 33.3 MB (35.91%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,25 +1,31 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        total_sum = sum(nums)
-        if total_sum % 2 != 0:
+        nums_sum = sum(nums)
+        if nums_sum % 2 != 0:
             return False
 
-        target = total_sum // 2
+        target = nums_sum // 2 
         n = len(nums)
-        dp = [[False] * (target + 1) for _ in range(n + 1)]
+
+        dp = [[False] * (target+1) for _ in range(n+1)]
         dp[0][0] = True
+
+        ## i is the ith number in nums
         for i in range(1, n+1):
-            cur = nums[i-1]
+            cur_num = nums[i-1]
 
-            for j in range(target + 1):
-                if j < cur:
+            # j is the jth sum in range(target)
+            for j in range(target+1):
+                if j < cur_num: 
+                    print(dp[i][j-1], i, j-1)
                     dp[i][j] = dp[i-1][j]
-
                 else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-cur] 
+                    dp[i][j] = dp[i-1][j] or dp[i-1][j-cur_num]
+
+        return dp[-1][-1]
 
 
-        #print(dp)
-        
-        return dp[n][target]
+                
+
+                
                 
