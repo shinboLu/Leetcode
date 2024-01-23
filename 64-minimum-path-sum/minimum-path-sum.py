0d8535492386ab627070218cd8ebaf517be8e251
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-22 23:10:31 -0800

    Time: 82 ms (85.11%) | Memory: 18.3 MB (62.51%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,26 +1,21 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        ## dp definition: 
-        ## dp[i][j] represents the sum of current min step to go right or down 
-        ##          to reach the bottom right corner
-        m, n = len(grid), len(grid[0])
+        nrow = len(grid) 
+        ncol = len(grid[0])
 
-        dp = [[0] * n for _ in range(m)]
+        dp = [[0] * (ncol) for _ in range(nrow)] 
         dp[0][0] = grid[0][0]
 
-        for i in range(1, m):
+
+        for i in range(1,nrow):
             dp[i][0] = dp[i-1][0] + grid[i][0]
 
-        for j in range(1, n):
+        for j in range(1, ncol):
             dp[0][j] = dp[0][j-1] + grid[0][j]
 
-        for i in range(1, m):
-            for j in range(1, n):
-                print(i,j)
+        for i in range(1, nrow):
+            for j in range(1, ncol):
                 dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
 
-        print(dp) 
-
-        return dp[m-1][n-1]
-
+        return dp[-1][-1]
         
