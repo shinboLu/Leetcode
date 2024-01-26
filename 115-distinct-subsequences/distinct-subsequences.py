5c919a8de5d20637f008c3c69c24a006bfbc3166
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-01-25 17:56:10 -0800

    Time: 307 ms (65.84%) | Memory: 73 MB (62.91%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.py
+++ 115-distinct-subsequences/distinct-subsequences.py
@@ -3,12 +3,14 @@ class Solution:
         nrow = len(s)
         ncol = len(t)
 
-        dp = [[0] * (ncol+1) for _ in range(nrow+1)]
+        dp = [[0] * (ncol +1) for _ in range(nrow+1)]
         for i in range(nrow+1):
-            dp[i][ncol] = 1   
+            dp[i][ncol] = 1
+
         for i in range(nrow-1, -1, -1):
             for j in range(ncol-1, -1, -1):
                 dp[i][j] = dp[i+1][j]
                 if s[i] == t[j]:
                     dp[i][j] += dp[i+1][j+1]
         return dp[0][0]
+
