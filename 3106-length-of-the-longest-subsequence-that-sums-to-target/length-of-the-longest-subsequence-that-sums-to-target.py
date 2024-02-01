fea1c6640c6a2f8ff8e1335f414ce3a682676063
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-01 14:32:05 -0800

    Time: 2362 ms (70.98%) | Memory: 17 MB (77.75%) - LeetSync

--- 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.py
+++ 3106-length-of-the-longest-subsequence-that-sums-to-target/length-of-the-longest-subsequence-that-sums-to-target.py
@@ -1,11 +1,13 @@
 class Solution:
     def lengthOfLongestSubsequence(self, nums: List[int], target: int) -> int:
-        dp = [float('-inf')] * (target+1)
+        ## need to decide nums[i] can contribute to the current solutions
 
+        dp = [float('-inf')] * (target+1)
         dp[0] = 0
 
-        for num in nums:
-            for i in range(target, num - 1, -1):
-                dp[i] = max(dp[i], dp[i - num] + 1)
+        for num in nums: 
+            for i in range(target, -1, -1):
+                if i - num >= 0:
+                    dp[i] = max(dp[i], dp[i-num]+1)
         print(dp)
         return dp[-1] if dp[-1]!=float('-inf') else -1 
