273a2304f305f07cbb0e37c85fa63ff5a01a276c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-03 22:49:28 -0800

    Time: 2180 ms (71.80%) | Memory: 16.5 MB (90.24%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -5,16 +5,15 @@ class Solution(object):
         :rtype: int
         """
 
-        root = [x ** 2 for x in range(0, int(math.sqrt(n))+1)]
+        roots = [x ** 2 for x in range(0, int(math.sqrt(n))+1)]
+        dp = [float('inf')]* (n+1)
+        dp[0] = 0
 
-
-        dp = [float('inf')] * (n+1)
-        dp[0] = 0 
 
         for i in range(1, n+1):
-            for j in root:
+            for j in roots:
                 if i < j:
                     break  
                 dp[i] = min(dp[i], dp[i-j] + 1)
 
-        return dp[-1] if dp[-1] != float('inf') else -1 
+        return dp[-1]
