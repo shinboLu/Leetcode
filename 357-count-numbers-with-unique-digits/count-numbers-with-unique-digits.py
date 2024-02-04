48c93ab0f91a8cce15c235f028a74ca0a79f27ff
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-03 23:45:21 -0800

    Time: 36 ms (63.54%) | Memory: 16.4 MB (72.80%) - LeetSync

--- 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.py
@@ -4,10 +4,11 @@ class Solution:
         if n == 1: return 10
 
         res = 10
-        temp = 9
+
+        prefix = 9
 
         for i in range(2, n+1):
-            temp *= (11-i)
-            res += temp
-
-        return res 
+            prefix *= (11-i)
+            res += prefix
+        
+        return res
