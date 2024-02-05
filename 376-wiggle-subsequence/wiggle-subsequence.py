2f619243051718059a94cd467927c9d5aa7bbc75
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-04 22:14:55 -0800

    Time: 35 ms (85.15%) | Memory: 16.5 MB (75.91%) - LeetSync

--- 376-wiggle-subsequence/wiggle-subsequence.py
+++ 376-wiggle-subsequence/wiggle-subsequence.py
@@ -1,14 +1,13 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
+        last_wiggle = None
         res = 1
         n = len(nums)
+        #longest sequence at idx
 
-        #last wiggle direction
-        last_wiggle = None
-
-        for i in range(1, n):
+        for i in range(1,n):
             cur_diff = nums[i] - nums[i-1]
-            if (cur_diff > 0 and last_wiggle != 1) or (cur_diff < 0 and last_wiggle != -1 ):
+            if (cur_diff >0 and last_wiggle != 1) or (cur_diff < 0 and last_wiggle != -1):
                 res += 1
                 last_wiggle = 1 if cur_diff > 0 else -1
 
