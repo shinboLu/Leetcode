15333e9bbef3f019e817fc0c1627338c613ba060
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-06 22:50:02 -0800

    Time: 43 ms (93.12%) | Memory: 16.8 MB (72.48%) - LeetSync

--- 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.py
+++ 1743-count-substrings-that-differ-by-one-character/count-substrings-that-differ-by-one-character.py
@@ -1,17 +1,20 @@
+
 class Solution:
-    def countSubstrings(self, s, t):
-        n, m = len(s), len(t)
-
-        match = [[0 for _ in range(m+1)] for _ in range(n+1)]
-        matchone = [[0 for _ in range(m+1)] for _ in range(n+1)]
-
-        for i in range(1,n+1):
-            for j in range(1,m+1):
-                if s[i-1] == t[j-1]:
-                    match[i][j] = 1 + match[i-1][j-1]
-                    matchone[i][j] = matchone[i-1][j-1]
+    def countSubstrings(self, s: str, t: str) -> int:
+        ans = 0
+        m, n = len(s), len(t)
+        f = [[0] * (n + 1) for _ in range(m + 1)]
+        g = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, a in enumerate(s, 1):
+            for j, b in enumerate(t, 1):
+                if a == b:
+                    f[i][j] = f[i - 1][j - 1] + 1
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if s[i] == t[j]:
+                    g[i][j] = g[i + 1][j + 1] + 1
                 else:
-                    match[i][j] = 0
-                    matchone[i][j] = 1 + match[i-1][j-1]
+                    ans += (f[i][j] + 1) * (g[i + 1][j + 1] + 1)
+        return ans
 
-        return sum([sum(i) for i in matchone])
+
