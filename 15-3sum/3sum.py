b826e9d610be56ac768bf56303273c429c489cb8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-12 16:10:42 -0800

    Time: 562 ms (87.96%) | Memory: 19.9 MB (98.35%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,30 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort() 
         res = []
+        def twoSum(i, res):
+            left, right = i+1, len(nums)-1
+            while left < right:
+                cur_sum = nums[i] + nums[left] + nums[right]
+                if cur_sum < 0:
+                    left += 1
+                elif cur_sum > 0:
+                    right -= 1
+                else:
+                    res.append((nums[i], nums[left], nums[right]))
+                    left += 1
+                    right -= 1
+
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
 
         for i in range(len(nums)):
             if nums[i] > 0:
-                break 
-
+                break  
             if i == 0 or nums[i-1] != nums[i]:
-                self.find2Sum(nums, res, i)
-        return res
-    def find2Sum(self, nums, res, i):
-        left,right = i+1, len(nums) -1
+                twoSum(i, res)
+        return res 
 
-        while left < right:
-            cur_sum = nums[i] + nums[left] + nums[right]
-            
-            if cur_sum < 0:
-                left += 1
-            elif cur_sum > 0:
-                right -= 1
-            else:
-                res.append([nums[i], nums[left], nums[right]])
-                left += 1
-                right -= 1
-                while left < right and nums[left] == nums[left-1]:
-                    left += 1
 
-            
