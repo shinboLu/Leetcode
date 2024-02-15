81e155e90b24f2c67070632c8effe074c9ff9cf1
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-14 18:57:45 -0800

    Time: 30 ms (87.70%) | Memory: 16.5 MB (77.95%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,20 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split(' ')
+        p_s_map = {}
+
+        if len(s) != len(pattern):
+            return False
+
+        for i in range(len(pattern)):
+            if s[i] in p_s_map.values():
+                continue
+            elif pattern[i] not in p_s_map:
+                p_s_map[pattern[i]] = s[i]
+
+        new_str = []
+
+        for p in pattern:
+            new_str.append(p_s_map.get(p, '0'))
+        print(p_s_map, new_str)
+        return new_str == s
