402bf12c12c5ff829ed50e77c8495c410b1250e5
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-18 21:59:27 -0800

    Time: 45 ms (22.36%) | Memory: 16.5 MB (79.25%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -5,18 +5,27 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1, l2):
-        res = ListNode(0)
-        prev = res
-        while l1 and l2:
-            if l1.val <= l2.val:
-                prev.next = l1
-                l1 = l1.next
-            else:
-                prev.next = l2 
-                l2 = l2.next
-            prev = prev.next
+        dummy_head = ListNode(0) 
+        cur = dummy_head
 
-        prev.next = l1 if l1 else l2
-        return res.next
+        while l1 and l2: 
+            if l1.val <= l2.val:
+                cur.next = l1 
+                l1 = l1.next
+
+            else:
+                cur.next = l2
+                l2 = l2.next
+
+            cur = cur.next
+        cur.next = l1 if l1 else l2
+
+        return dummy_head.next
+
+         
                 
-            
+
+        
+        
+        
+        
