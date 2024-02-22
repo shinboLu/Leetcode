3e1306728ef60b93b6d8719e520fc67894728f52
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-21 22:48:43 -0800

    Time: 704 ms (75.81%) | Memory: 64.8 MB (58.85%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,14 +1,15 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         res = []
-        def bt(index, combs):
+
+        def bt(idx, combs):
             if len(combs) == k:
                 res.append(combs.copy())
-                return 
+                return
 
-            for i in range(index, n+1):
+            for i in range(idx, n+1):
                 combs.append(i)
                 bt(i+1, combs)
                 combs.pop()
-        bt(1,[])
-        return res 
+        bt(1, [])
+        return res
