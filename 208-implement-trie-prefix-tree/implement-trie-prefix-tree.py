8c9e0f791f6f1799158ed1e3467484338e8af5bb
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-27 18:37:16 -0800

    Time: 143 ms (30.92%) | Memory: 33.9 MB (14.75%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,24 +1,42 @@
-class Trie:
-
+class TrieNode:
     def __init__(self):
-        self.root = []
+        self.children = [None] * 26
+        self.is_end = False
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
-        self.root.append(word)
-
+        node = self.root 
+        for char in word:
+            idx = ord(char) - ord('a')
+            if node.children[idx] is None:
+                node.children[idx] = TrieNode()
+            node = node.children[idx] 
+        node.is_end = True
+    
     def search(self, word: str) -> bool:
-        if word in self.root:
+        node = self.root
+        for ch in word:
+            idx = ord(ch) - ord('a')
+            if node.children[idx] is None:
+                return False
+            node = node.children[idx]
+        if node.is_end == True:
             return True
         return False
 
-    def startsWith(self, prefix: str) -> bool:
-        n = len(prefix)
 
-        for w in self.root:
-            if w[:n] == prefix:
-                return True
-        return False
-        
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            idx = ord(char) - ord('a')
+            if node.children[idx] == None:
+                return False
+            node = node.children[idx] 
+
+        return True 
+
 
 
 # Your Trie object will be instantiated and called as such:
