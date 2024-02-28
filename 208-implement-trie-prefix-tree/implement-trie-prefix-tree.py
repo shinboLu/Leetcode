bda3ee09bcb6a85b34514f00729fce9ccf9ade45
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-27 23:15:20 -0800

    Time: 135 ms (41.64%) | Memory: 31.7 MB (36.15%) - LeetSync

--- 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
+++ 208-implement-trie-prefix-tree/implement-trie-prefix-tree.py
@@ -1,42 +1,43 @@
 class TrieNode:
     def __init__(self):
-        self.children = [None] * 26
+        self.children = {}
         self.is_end = False
+
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode() 
 
     def insert(self, word: str) -> None:
-        node = self.root 
-        for char in word:
-            idx = ord(char) - ord('a')
-            if node.children[idx] is None:
-                node.children[idx] = TrieNode()
-            node = node.children[idx] 
-        node.is_end = True
-    
-    def search(self, word: str) -> bool:
-        node = self.root
+        cur = self.root
         for ch in word:
             idx = ord(ch) - ord('a')
-            if node.children[idx] is None:
-                return False
-            node = node.children[idx]
-        if node.is_end == True:
-            return True
-        return False
+            if cur.children.get(idx, -1) == -1:
+                cur.children[idx] = TrieNode()
+            cur = cur.children[idx]
+        
+        cur.is_end = True
+        
 
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for ch in word:
+            idx = ord(ch) - ord('a')
+            if cur.children.get(idx, -1) == -1:
+                return False 
+            cur = cur.children[idx]
+        if cur.is_end == True:
+            return True
 
     def startsWith(self, prefix: str) -> bool:
-        node = self.root
+        cur = self.root
         for char in prefix:
             idx = ord(char) - ord('a')
-            if node.children[idx] == None:
+            if cur.children.get(idx, -1) == -1:
                 return False
-            node = node.children[idx] 
 
-        return True 
-
+            cur = cur.children[idx]
+        return True
+        
 
 
 # Your Trie object will be instantiated and called as such:
