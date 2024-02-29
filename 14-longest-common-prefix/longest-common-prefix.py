9c41069eea81016a69dd6fe41fdba6d3586f5aa7
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-28 22:10:54 -0800

    Time: 44 ms (21.99%) | Memory: 16.7 MB (63.14%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,10 +1,54 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        cur = self.root
+        for ch in word:
+            if ch not in cur.children:
+                cur.children[ch] = TrieNode()
+            cur = cur.children[ch] 
+        cur.is_end = True
+
 class Solution:
+    
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        base = strs[0]
+        if not strs:
+            return '' 
 
-        for i in range(len(base)):
-            for word in strs[1:]:
-                if i == len(word) or word[i] != base[i]:
-                    return base[:i] 
+        trie = Trie()
 
-        return base 
+        for word in strs:
+            trie.insert(word)
+
+        cur = trie.root
+
+        res = ''
+
+        while cur:
+            if len(cur.children) > 1 or cur.is_end:
+                return res 
+
+            key = list(cur.children)[0] 
+            res += key 
+
+            cur = cur.children[key]
+
+        return res 
+
+            
+
+        return ''
+
+        
+
+
+        
+
+        
+
