99cb95c1651286b8937b3c22599a92b532bf3741
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-02-29 18:28:52 -0800

    Time: 190 ms (76.08%) | Memory: 21.4 MB (5.21%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.py
@@ -1,18 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_word = False
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        def buildTree(dictionary):
+            root = TrieNode()
+
+            for word in dictionary:
+                node = root
+                for char in word:
+                    if char not in node.children:
+                        node.children[char] = TrieNode()
+                    node = node.children[char] 
+                node.is_word = True
+            return root
+
+        root = buildTree(dictionary)
         n = len(s) 
-        #memo = {} 
 
         @cache
         def dp(idx):
             if idx == n:
                 return 0
-
+            nonlocal root 
+            cur = root
             res = dp(idx+1) + 1
             
             for end in range(idx, n):
-                cur = s[idx:end+1]
-                if cur in dictionary:
+                if s[end] not in cur.children:
+                    break  
+                cur = cur.children[s[end]]
+                if cur.is_word:
                     res = min(res, dp(end+1))
             return res 
+
+
         return dp(0)
