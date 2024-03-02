2b439a34d6fe182fc6677d117391188751148075
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-01 22:28:34 -0800

    Time: 37 ms (53.51%) | Memory: 16.6 MB (46.75%) - LeetSync

--- 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.py
@@ -1,9 +1,17 @@
+from heapq import heappush, heappop
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        factors = [] 
 
-        for i in range(1, n+1):
-            if n%i == 0:
-                factors.append(i) 
+        def heappush_k(num):
+            heappush(heap, -num) 
+            if len(heap) > k:
+                heappop(heap)  
 
-        return factors[k-1] if len(factors) >= k else -1
+        heap = []
+
+        for x in range(1, int(n**0.5)+1):
+            if n % x == 0:
+                heappush_k(x) 
+                if x != n // x:
+                    heappush_k(n//x)
+        return -heappop(heap) if k == len(heap) else -1 
