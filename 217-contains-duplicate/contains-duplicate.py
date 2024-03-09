2a8e5aa77ce61c94a42288ed9c3b4ba278babc89
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-09 13:29:01 -0800

    Time: 452 ms (11.93%) | Memory: 34.8 MB (6.90%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,9 @@
+from heapq import heapify, heappop
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        counter = collections.Counter(nums)
+
+        for k, v in counter.items():
+            if v >= 2:
+                return True
+        return False  
