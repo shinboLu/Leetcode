39f8985717ecd0a5689f1ea67c808df6972b8782
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-10 20:56:37 -0700

    Time: 58 ms (39.75%) | Memory: 20.3 MB (87.55%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,17 +7,18 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def dfs(root, val1, val2):
+        def find(root, val1, val2):
             if not root:
-                return 
-
-            if root.val > val2:
-                return dfs(root.left, val1, val2)
-            elif root.val < val1:
-                return dfs(root.right,val1, val2)
-            else:
+                return None
+            # 前序位置
+            if root.val == val1 or root.val == val2:
+                # 如果遇到目标值，直接返回
                 return root
-        
-        val1 = min(p.val, q.val) 
-        val2 = max(p.val, q.val)
-        return dfs(root, val1, val2)
+            left = find(root.left, val1, val2)
+            right = find(root.right, val1, val2)
+            # 后序位置，已经知道左右子树是否存在目标值
+            if left and right:
+                # 当前节点是 LCA 节点
+                return root
+            return left if left else right
+        return find(root, p.val, q.val)
