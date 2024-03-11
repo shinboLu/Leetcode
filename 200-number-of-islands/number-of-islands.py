221db55c8a99e0623921e82d0cc616c557b16aa7
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-11 14:47:02 -0700

    Time: 278 ms (28.53%) | Memory: 24 MB (41.40%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,38 +1,38 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        dirs = [[1,0], [0,1], [-1,0], [0, -1]]
-        visited = set()  
-        res = 0
+        dirs = [[0, 1], [1, 0], [-1,0], [0, -1]]
         nrow = len(grid)
-        ncol = len(grid[0])
-
-        def getNeighbors(x, y):
-            neighbors = []
-            for dx, dy in dirs:
-                nx, ny = x + dx, y + dy  
-                if 0 <= nx < nrow and 0 <= ny < ncol:
-                    neighbors.append((nx, ny))
-            return neighbors 
-
+        ncol = len(grid[0]) 
+        visited = set()
         
+        def get_nei(x,y):
+            neigh = []
+            for dx, dy in dirs:
+                nx, ny = dx+x, dy+y
+                if 0<=nx<nrow and 0 <= ny < ncol:
+                    neigh.append((nx, ny)) 
+
+            return neigh
+
         def bfs(x, y):
-            queue = collections.deque()
+            queue = collections.deque() 
             queue.append((x, y))
 
+            
             while queue:
-                x, y = queue.popleft()
-                for nx, ny in getNeighbors(x, y):
-                    if (nx, ny) not in visited and grid[nx][ny] != '0':
-                        queue.append((nx, ny))
+                cur_x, cur_y = queue.popleft()
+                for nx, ny in get_nei(cur_x, cur_y):
+                    if (nx,ny) not in visited and grid[nx][ny] == "1":
                         visited.add((nx, ny))
-        
-        for x in range(nrow):
-            for y in range(ncol):
-                if grid[x][y] == '1' and (x, y) not in visited:
+                        queue.append((nx, ny))
+        res = 0
+        for row in range(nrow):
+            for col in range(ncol):
+                if grid[row][col] == '1' and (row, col) not in visited:
+                    bfs(row, col)
                     res += 1
-                    bfs(x, y)
+        return res  
 
-        return res 
 
 
 
