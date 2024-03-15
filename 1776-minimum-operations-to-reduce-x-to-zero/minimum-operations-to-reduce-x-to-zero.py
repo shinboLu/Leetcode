d0b1902e079a8b62a83e9f2874b7c128265db5ef
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-14 20:00:16 -0700

    Time: 846 ms (46.80%) | Memory: 30.5 MB (90.80%) - LeetSync

--- 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/minimum-operations-to-reduce-x-to-zero.py
@@ -1,22 +1,21 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        total = sum(nums) 
-        n = len(nums) 
-        left = 0 
-        prefix_sum = 0 
-        max_len = -1
+        tot_sum = sum(nums) 
+        
+        left = 0
+        res = float('inf')
 
-        for right in range(n):
-            prefix_sum += nums[right] 
-            while prefix_sum > total - x and left <= right:
-                prefix_sum -= nums[left] 
-                left += 1 
+        for right in range(len(nums)):
+            tot_sum -= nums[right] 
+
+            while tot_sum < x and left <= right:
+                
+                tot_sum += nums[left] 
+                left += 1
+
+            if tot_sum == x:
+                res = min(res, (len(nums)-1-right) + left)
+
+        return res if res != float('inf') else -1 
+
             
-            if prefix_sum == total -x:
-                max_len = max(max_len, right - left + 1)
-
-        return n-max_len if max_len != -1 else -1 
-
-            
-
-
