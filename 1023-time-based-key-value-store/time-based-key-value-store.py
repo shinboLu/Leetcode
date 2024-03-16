db94687a1a1c6e413b91859d9809b61175862042
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-16 14:40:49 -0700

    Time: 496 ms (95.52%) | Memory: 71 MB (95.16%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,23 +1,24 @@
-from sortedcontainers import SortedDict
+'''
+Time: O(log k), space:O(n),对于值和时间戳分别使用两个dict，其value是一个可拓展的list
+'''
 class TimeMap:
 
     def __init__(self):
-        self.map = {}
+        self.meta = collections.defaultdict(list)
+        self.data = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.map:
-            self.map[key] = SortedDict()
-        self.map[key][timestamp]= value
-        
+        self.meta[key].append(timestamp)
+        self.data[key].append(value)
+
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.map:
-            return ""
-
-        iterator = self.map[key].bisect_right(timestamp)
-        if iterator == 0:
-            return ""
-
-        return self.map[key].peekitem(iterator-1)[1]
+        # 因为时间戳是已经sorted，所以可以用bisect
+        #bisect([3, 5, 8], 4) 在已sorted的array插入4时，4的idx = 1
+        idx = bisect.bisect(self.meta[key], timestamp)
+        if idx == 0:
+            return ''
+        # index is the first element that is greater than the requested value. 需-1
+        return self.data[key][idx - 1]
 
 
 # Your TimeMap object will be instantiated and called as such:
