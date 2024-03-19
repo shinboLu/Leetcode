8b0da54d95ab6bfc770290b88a2eef73de13542e
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-19 10:41:50 -0700

    Time: 39 ms (24.95%) | Memory: 16.5 MB (88.30%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         dp = [0] * (n+1)
-        dp[0] = dp[1] = 1
+
+        dp[0] = 1
+        dp[1] = 1
+        ## dp[i] number of ways to climb
+
         for i in range(2, n+1):
-            dp[i] = dp[i-1] + dp[i-2]
+            dp[i] = dp[i-1] + dp[i-2] 
 
         return dp[-1]
+            
+
+
+        
