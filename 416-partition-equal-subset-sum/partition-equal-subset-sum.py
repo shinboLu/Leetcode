8b7d831013da7448c36b56ea0d264fdbcc7c43cb
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-20 17:36:43 -0700

    Time: 1502 ms (27.12%) | Memory: 32.2 MB (41.13%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,25 +1,23 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        nums_sum = sum(nums)
-        if nums_sum % 2 != 0:
+        tot_sum = sum(nums)
+        # 和为奇数时，不可能划分成两个和相等的集合
+        if tot_sum % 2 != 0:
             return False
-        target = nums_sum // 2 
         n = len(nums)
-        dp = set()
-        dp.add(0)
+        tot_sum = tot_sum // 2
+        # 定义二维数组 dp
+        dp = [[False] * (tot_sum + 1) for _ in range(n + 1)]
+        # base case
+        for i in range(n + 1):
+            dp[i][0] = True
 
-        for i in range(len(nums)-1, -1, -1):
-            cur_num = nums[i] 
-            nextDp = set()
-            for t in dp:
-                nextDp.add(t)
-                nextDp.add(t + cur_num)
-            dp = nextDp
-        return target in dp
-                
-
-
-                
-
-                
-                
+        for i in range(1, n + 1):
+            for j in range(1, tot_sum + 1):
+                if j - nums[i - 1] < 0:
+                    # 背包容量不足，不能装入第 i 个物品
+                    dp[i][j] = dp[i - 1][j]
+                else:
+                    # 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] or dp[i - 1][j - nums[i - 1]]
+        return dp[n][tot_sum]
