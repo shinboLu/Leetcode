f497081616e68e51f08203e4f0065339580b0b3c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-21 19:20:24 -0700

    Time: 49 ms (38.63%) | Memory: 16.7 MB (57.52%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -2,21 +2,19 @@ class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         dp = {}
 
-        def dfs(idx): 
-            if idx == len(days):
+        def dfs(i):
+            if i == len(days):
                 return 0
-            if idx in dp:
-                return dp[idx]
+            if i in dp:
+                return dp[i]
+            dp[i] = float('inf')
+                ## j: determine the next day we need to purchase a ticket
+            j = i
+            for d,c in zip([1,7,30], costs):
+                ## check the next purchase date is in bound, and is smaller that the current day + the ticket validate days 
+                while j < len(days) and days[j] < days[i] + d:
+                    j += 1
+                dp[i] = min(dp[i], c + dfs(j))
+            return dp[i]
 
-            dp[idx] = float('inf')
-            for d, c in zip([1,7,30], costs):
-                j = idx
-                while j < len(days) and days[j] < days[idx] + d:
-                    j+=1
-
-                dp[idx] = min(dp[idx], c + dfs(j))
-            return dp[idx]
         return dfs(0)
-
-
-        return 0
