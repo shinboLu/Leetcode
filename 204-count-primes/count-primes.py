b53900abf552f34158e39601c4dac762cf9eb6da
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-03-31 18:43:38 -0700

    Time: 1573 ms (62.97%) | Memory: 55.3 MB (54.21%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,18 +1,14 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        if n <= 2:
-            return 0
-        
-        # Initialize numbers[0] and numbers[1] as False because 0 and 1 are not prime.
-        # Initialze numbers[2] through numbers[n-1] as True because we assume each number
-        # is prime until we find a prime number (p) that is a divisor of the number
-        numbers = [False, False] + [True] * (n - 2)
-        for p in range(2, int(sqrt(n)) + 1):
-            if numbers[p]:
-                # Set all multiples of p to false because they are not prime.
-                for multiple in range(p * p, n, p):
-                    numbers[multiple] = False
-        
-        # numbers[index] will only be true where index is a prime number
-        # return the number of indices whose value is true.
-        return sum(numbers)
+        isPrime = [True] * n
+        for i in range(2, int(n ** 0.5) + 1):
+            if isPrime[i]:
+                for j in range(i * i, n, i):
+                    isPrime[j] = False
+
+        count = 0
+        for i in range(2, n):
+            if isPrime[i]: 
+                count += 1
+
+        return count
