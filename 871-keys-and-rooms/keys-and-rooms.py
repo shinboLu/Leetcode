ac7b34ff094621e26e72aa218adb17be8369cda0
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-04-10 22:53:21 -0700

    Time: 67 ms (47.49%) | Memory: 17.1 MB (60.33%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.py
+++ 871-keys-and-rooms/keys-and-rooms.py
@@ -1,27 +1,19 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        print(len(rooms))
-        graph = collections.defaultdict(list)
-        indegree = [0] * len(rooms)
-        for idx, val in enumerate(rooms):
-            for element in val:
-                graph[idx].append(element)
+        visited = [False] * len(rooms)
+        visited[0] = True 
 
-        queue = collections.deque()
-        queue.append(0)
-        visited = set()
-        unlocked_room = 0 
-        
+        stack = [0]
 
-        while queue:
-            #print(queue)
-            cur_room = queue.popleft()
-            for next_room in graph[cur_room]:
-                if next_room not in visited and cur_room != next_room or not next_room: 
-                    queue.append(next_room)
-                    visited.add(next_room)
-                    unlocked_room +=1
-                    print(unlocked_room)
+        while stack:
+            node = stack.pop(0)
 
-        return True if unlocked_room == len(rooms)-1 else False
+            for nei in rooms[node]:
+                if not visited[nei]:
+                    visited[nei] = True 
+                    stack.append(nei) 
+
+        return all(visited)
+
+            
 
