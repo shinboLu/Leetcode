a3e464d69fe0134c2e1af181b25992156fa2690f
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-04-15 20:13:26 -0700

    Time: 29 ms (99.37%) | Memory: 16.5 MB (97.81%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.py
+++ 360-sort-transformed-array/sort-transformed-array.py
@@ -1,8 +1,35 @@
 class Solution:
     def sortTransformedArray(self, nums: List[int], a: int, b: int, c: int) -> List[int]:
-        res = []
-        for n in nums:
-            mult = a * (n**2) + b * n + c
+        def transform(x):
+            # Return the transformed result for element 'x'
+            return (a * x * x) + (b * x) + c
 
-            res.append(mult)
-        return sorted(res)
+        res = [] 
+        left = 0
+        right = len(nums)-1
+
+        if a < 0:
+            while left <= right:
+                left_val = transform(nums[left])
+                right_val = transform(nums[right])
+
+                if left_val < right_val:
+                    res.append(left_val)
+                    left += 1
+                else:
+                    res.append(right_val)
+                    right-=1 
+        else:
+            while left <= right:
+                left_val = transform(nums[left])
+                right_val = transform(nums[right])
+                if left_val > right_val:
+                    res.append(left_val)
+                    left += 1
+                else:
+                    res.append(right_val)
+                    right -= 1
+            res.reverse() 
+
+        return res 
+
