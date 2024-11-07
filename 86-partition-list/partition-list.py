1a0e939a0bbc25a7a230e1d9fb5ee5445b34c0c4
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-11-06 22:12:44 -0800

    Time: 0 ms (100.00%) | Memory: 16.6 MB (67.08%) - LeetSync

--- 86-partition-list/partition-list.py
+++ 86-partition-list/partition-list.py
@@ -5,25 +5,26 @@
 #         self.next = next
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-        before_head = ListNode(0)
-        after_head = ListNode(0)
+        small = ListNode(-1) 
+        big = ListNode(-1)
+        cur_s = small
+        cur_b = big
+        p1 = head
 
-        before_p = before_head
-        after_p = after_head
-
-        while head:
-            if head.val < x:
-                before_p.next = head
-                before_p = before_p.next 
-
+        while p1:
+            if p1.val < x:
+                cur_s.next = p1
+                cur_s = cur_s.next
             else:
-                after_p.next = head  
-                after_p = after_p.next 
+                cur_b.next = p1
+                cur_b = cur_b.next
+            temp = p1.next
+            p1.next = None
+            p1 = temp
+        
+        cur_s.next = big.next
+        return small.next
+            
 
-            head = head.next 
-
-        after_p.next = None 
-
-        before_p.next = after_head.next
-
-        return before_head.next
+            
+                
