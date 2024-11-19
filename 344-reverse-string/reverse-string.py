3477d1f3067da5b2bbc439d500a9269c6ae47fa9
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2024-11-18 21:41:56 -0800

    Time: 0 ms (100.00%) | Memory: 22.1 MB (19.98%) - LeetSync

--- 344-reverse-string/reverse-string.py
+++ 344-reverse-string/reverse-string.py
@@ -3,10 +3,9 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-
-        def recursion(left, right):
-            if left<right:
-                s[left], s[right] = s[right], s[left]
-                recursion(left+1, right-1)
-
-        recursion(0, len(s)-1)
+        left = 0 
+        right = len(s)-1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left += 1
+            right -=1
