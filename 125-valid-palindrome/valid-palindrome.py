567d91a2525d13d8555411b9238055603cb5008f
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-01-26 20:29:56 -0800

    Time: 11 ms (36.79%) | Memory: 18.3 MB (41.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,26 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = s.replace(' ','')
+        ns = ''
+        for c in s:
+            if c.isalnum():
+                ns+=c.lower()
         left = 0
-        right = len(s)-1
+        right = len(ns)-1
 
         while left < right:
-            left_char = s[left].lower()
-            right_char = s[right].lower()
-            if left_char.isalnum() and right_char.isalnum():
-                if left_char == right_char:
-                    left+=1
-                    right-=1
-                    continue
-                else:
-                    return False
-            elif not left_char.isalnum() and right_char.isalnum():
-                left+=1
-            elif not left_char.isalnum() and not right_char.isalnum():
-                left+=1
-                right-=1
+            if ns[left] == ns[right]:
+                left += 1
+                right -=1 
             else:
-                right-=1
-            
+                return False
 
         return True
