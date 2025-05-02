5a907c6e3041e7a910b9c60a8f683349c47078a8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-05-01 20:47:28 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (6.31%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,9 @@
-from collections import defaultdict
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping = defaultdict(int)
+        sum_dict = {}
 
-        for idx, val in enumerate(nums):
-            remains = target-val
-
-            if remains in mapping.keys():
-                return [idx, mapping[remains]]
-            
-            mapping[val] = idx
-
-        
+        for i in range(len(nums)):
+            if nums[i] in sum_dict:
+                return [sum_dict[nums[i]], i]
+            else:
+                sum_dict[target - nums[i]] = i
