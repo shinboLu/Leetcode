d7ef20a60be216cd2858ea6de8a3fb33cb42f845
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-05-04 22:13:40 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (17.17%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,23 +1,28 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        stack = []
-
-        operations = {
-            '+': lambda a, b: a+b, 
-            '-': lambda a, b: a - b, 
-            '*' : lambda a, b: a * b,  
-            '/' : lambda a, b: int(a/b)  
-        }
-
+        def calc(v1,v2,o):
+            if o == '+':
+                return v1 + v2
+            if o == '*':
+                return v1 * v2
+            if o == '/':
+                return int(v1/v2)
+            if o =='-':
+                return v1-v2
+        nums = []
+        oper = ["+", '-', '*', '/']
         for token in tokens:
-            if token in operations:
-                num2  = stack.pop()
-                num1 = stack.pop()
+            if token not in oper:
+                nums.append(token)
+                continue
+            
+            if token in oper and len(nums) >=2:
+                num1 = int(nums.pop())
+                num2 = int(nums.pop())
+                cur_res = calc(num2, num1, token)
+                nums.append(int(cur_res))
 
-                operation = operations[token]
-                stack.append(operation(num1, num2))
+        return int(nums.pop())
 
-            else: 
-                stack.append(int(token))
 
-        return stack.pop()
+                    
