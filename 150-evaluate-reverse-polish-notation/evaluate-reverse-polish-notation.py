a56bbea15708063d326b1d2643b0d9f675ca76e5
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-05-07 22:49:00 -0700

    Time: 4 ms (29.85%) | Memory: 19.1 MB (88.12%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.py
@@ -1,28 +1,27 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        def calc(v1,v2,o):
-            if o == '+':
-                return v1 + v2
-            if o == '*':
-                return v1 * v2
-            if o == '/':
-                return int(v1/v2)
-            if o =='-':
-                return v1-v2
-        nums = []
-        oper = ["+", '-', '*', '/']
-        for token in tokens:
-            if token not in oper:
-                nums.append(token)
-                continue
-            
-            if token in oper and len(nums) >=2:
+
+        def calc(num0, num1, oper):
+            if oper == '*':
+                return num0 * num1
+            elif oper == '+':
+                return num0 + num1
+            elif oper == '-':
+                return num1 - num0
+            else:
+                return int(num1/num0)
+
+        opers = ['*', '+', '-', '/']
+
+        nums = [] 
+
+        for tok in tokens:
+            if tok not in opers:
+                nums.append(tok)
+            else:
                 num1 = int(nums.pop())
-                num2 = int(nums.pop())
-                cur_res = calc(num2, num1, token)
-                nums.append(int(cur_res))
+                num0 = int(nums.pop())
+                cur = calc(num1, num0, tok)
+                nums.append(cur) 
 
-        return int(nums.pop())
-
-
-                    
+        return int(nums[-1])
