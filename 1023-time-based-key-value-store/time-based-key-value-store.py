b64e0ca5a318b0a158261953fba67f12e256a919
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-05-29 21:09:31 -0700

    Time: 112 ms (69.25%) | Memory: 75.7 MB (13.06%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.py
+++ 1023-time-based-key-value-store/time-based-key-value-store.py
@@ -1,24 +1,23 @@
-'''
-Time: O(log k), space:O(n),对于值和时间戳分别使用两个dict，其value是一个可拓展的list
-'''
 class TimeMap:
 
     def __init__(self):
-        self.meta = collections.defaultdict(list)
-        self.data = collections.defaultdict(list)
+        self.store = collections.defaultdict(list)
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        self.meta[key].append(timestamp)
-        self.data[key].append(value)
+        self.store[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        # 因为时间戳是已经sorted，所以可以用bisect
-        #bisect([3, 5, 8], 4) 在已sorted的array插入4时，4的idx = 1
-        idx = bisect.bisect(self.meta[key], timestamp)
-        if idx == 0:
-            return ''
-        # index is the first element that is greater than the requested value. 需-1
-        return self.data[key][idx - 1]
+        left = 0
+        right = len(self.store[key])
+
+        while left < right:
+            mid = (left+right)//2
+            if self.store[key][mid][1] <= timestamp:
+                left = mid + 1
+            else:
+                right = mid
+        return self.store[key][right-1][0] if right !=0 else ""
+
 
 
 # Your TimeMap object will be instantiated and called as such:
