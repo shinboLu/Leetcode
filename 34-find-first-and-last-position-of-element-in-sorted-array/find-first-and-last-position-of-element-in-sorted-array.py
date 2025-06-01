ea910c54d30bf64951e49d84ba97701014e60bfc
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-05-31 22:49:19 -0700

    Time: 0 ms (100.00%) | Memory: 19 MB (52.70%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,33 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        left = self.left_bound(nums, target)
-        right = self.right_bound(nums, target)
-        return [left, right]
-    def left_bound(self, nums, target):
         left, right = 0, len(nums)-1
+
+        left = self.getLeft(nums, target)
+        right = self.getRight(nums, target)
+        return [left, right]
+    def getLeft(self, nums, target):
+        left = 0
+        right = len(nums)-1
+
         while left <= right:
-            mid = (left+right)//2
+            mid = (left+right)//2 
             if nums[mid] < target:
                 left = mid + 1
             elif nums[mid] >= target:
                 right = mid -1
         if left >= len(nums) or nums[left] != target:
-            return -1 
+            return -1
         return left
-    def right_bound(self, nums, target):
-        left, right = 0, len(nums)-1
-        while left <= right:
-            mid = (left+right)//2
+
+    def getRight(self,nums, target):
+        left = 0
+        right = len(nums)-1
+        while left <= right: 
+            mid = (left+right)//2 
             if nums[mid] < target:
                 left = mid + 1
-            elif nums[mid] > target:
-                right = mid -1
             elif nums[mid] == target:
                 left = mid + 1
-        if right < 0 or nums[right] != target:
-            return -1 
-        return right
-    
+            if nums[mid] > target:
+                right = mid -1
 
-            
-                
+        if right < 0 or nums[right] != target:
+            return -1
+        return right
+
+
