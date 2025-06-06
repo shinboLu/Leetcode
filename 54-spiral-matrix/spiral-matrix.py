c841ce2170642e082c0aadaf1ea67ee124a04027
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-06-05 23:43:06 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (18.33%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,34 +1,36 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        nrow = len(matrix)
-        ncol = len(matrix[0])
-        visited = 101
-        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        res = [] 
 
-        cur_direction = 0
-        change_direction = 0
+        m, n = len(matrix), len(matrix[0])
+        upper_bound, lower_bound = 0, m-1
+        left_bound, right_bound = 0, n-1
+
+        # m=3, upper_bound = 0, lower_bound = 2
+        # n = 4, left_bound = 0, right_bound = 3
+
+        while len(res) < m*n:
+            if upper_bound <= lower_bound:
+                for i in range(left_bound, right_bound+1):
+                    res.append(matrix[upper_bound][i]) 
+                upper_bound+=1
+            
+            if left_bound <= right_bound:
+                for i in range(upper_bound, lower_bound+1):
+                    res.append(matrix[i][right_bound])
+                right_bound -=1 
+
+            if upper_bound <= lower_bound:
+                for i in range(right_bound, left_bound-1, -1):
+                    res.append(matrix[lower_bound][i]) 
+                lower_bound -=1 
+            
+            if left_bound <= right_bound:
+                for i in range(lower_bound, upper_bound-1, -1):
+                    res.append(matrix[i][left_bound])
+                left_bound+=1
         
-        cur_row, cur_col = 0,0
+        return res
+            
 
-        res = [matrix[0][0]]
 
-        matrix[cur_row][cur_col] = visited
-
-        while change_direction < 2:
-            while True:
-                next_row = cur_row + directions[cur_direction][0]
-                next_col = cur_col + directions[cur_direction][1] 
-
-                if not (0<=next_row < nrow and 0 <= next_col <ncol):
-                    break
-                if matrix[next_row][next_col] == visited:
-                    break
-
-                change_direction = 0
-                cur_row, cur_col = next_row, next_col 
-                res.append(matrix[cur_row][cur_col])
-                matrix[cur_row][cur_col] = visited
-
-            cur_direction = (cur_direction + 1) % 4
-            change_direction +=1
-        return res 
