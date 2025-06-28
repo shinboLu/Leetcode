3398ac977c85f7fb4186241bfb9336f3c581eaed
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-06-27 22:18:33 -0700

    Time: 0 ms (100.00%) | Memory: 17.7 MB (58.43%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -6,18 +6,18 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
+
         def dfs_return_sum(node, cur_sum):
             if not node:
                 return 0
-
+            
             cur_sum = cur_sum * 10 + node.val
 
             if not node.left and not node.right:
                 return cur_sum
-
-            left_sum = dfs_return_sum(node.left, cur_sum)
-            right_sum = dfs_return_sum(node.right, cur_sum)
-
-            return left_sum + right_sum
-        return dfs_return_sum(root, 0)
             
+            left = dfs_return_sum(node.left, cur_sum)
+            right = dfs_return_sum(node.right, cur_sum)
+
+            return left + right
+        return dfs_return_sum(root, 0)
