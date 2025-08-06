0fe8361252a5fe468efb3f6d1e76bda3471716ae
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-08-06 00:06:42 -0700

    Time: 3 ms (48.45%) | Memory: 18.9 MB (66.58%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,39 +1,25 @@
-class UnionFind:
-    def __init__(self, size):
-        self.root = [x for x in range(size)]
-        self.rank = [1] * size 
-
-    def find(self, x):
-        if self.root[x] == x:
-            return x 
-        self.root[x] = self.find(self.root[x])
-        return self.root[x] 
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-
-        if root_x == root_y:
-            return False
-        
-        if self.rank[root_x] > self.rank[root_y]:
-            self.root[root_y] = root_x
-        elif self.rank[root_x] < self.rank[root_y]:
-            self.root[root_x] = root_y
-        else:
-            self.root[root_y] =root_x
-            self.rank[root_x] += 1
-        return True 
-
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) != n-1:
-            return False
+        graph = collections.defaultdict(list)
+        for in_node, out_node in edges:
+            graph[in_node].append(out_node)
+            graph[out_node].append(in_node)
 
-        uf = UnionFind(n)
+        def bfs(n, edges, graph):
+            queue = collections.deque([0])
+            visited = set()
 
-        for x, y in edges:
-            if not uf.union(x, y):
-                return False
-        return True
+            parent = {}
+
+            while queue:
+                cur = queue.popleft()
+                visited.add(cur)
+                for child in graph[cur]:
+                    if parent.get(cur) == child:
+                        continue
+                    if child in visited:
+                        return False
+                    parent[child] = cur
+                    queue.append(child)
+            return len(visited)==n
+        return bfs(n, edges, graph)
