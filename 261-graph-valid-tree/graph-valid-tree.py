a9362df3c622759684ff4e7c5c5a557acbef5fc7
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-08-17 06:52:35 -0700

    Time: 0 ms (100.00%) | Memory: 18.9 MB (84.44%) - LeetSync

--- 261-graph-valid-tree/graph-valid-tree.py
+++ 261-graph-valid-tree/graph-valid-tree.py
@@ -1,25 +1,39 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [x for x in range(size)]
+        self.rank = [1 for x in range(size)]
+    
+    def find(self, x):
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if self.rank[root_x] > self.rank[root_y]:
+            self.root[root_y] = root_x
+        elif self.rank[root_x] < self.rank[root_y]:
+            self.root[root_x] = root_y
+        else:
+            self.root[root_y] = root_x
+            self.rank[root_x] += 1
+    def is_connected(self, x,y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        graph = collections.defaultdict(list)
-        for in_node, out_node in edges:
-            graph[in_node].append(out_node)
-            graph[out_node].append(in_node)
+        # A valid tree must have exactly n-1 edges
+        if len(edges) != n - 1:
+            return False
+        
+        uf = UnionFind(n)
 
-        def bfs(n, edges, graph):
-            queue = collections.deque([0])
-            visited = set()
-
-            parent = {}
-
-            while queue:
-                cur = queue.popleft()
-                visited.add(cur)
-                for child in graph[cur]:
-                    if parent.get(cur) == child:
-                        continue
-                    if child in visited:
-                        return False
-                    parent[child] = cur
-                    queue.append(child)
-            return len(visited)==n
-        return bfs(n, edges, graph)
+        for n1, n2 in edges:
+            # if already connected, we found a cycle
+            if uf.is_connected(n1, n2):
+                return False
+            uf.union(n1, n2)
+        
+        return True
