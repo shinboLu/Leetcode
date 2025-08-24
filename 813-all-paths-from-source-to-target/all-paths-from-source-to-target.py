4b6072266f6e662ea87c7c1006a61bf043d327e6
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-08-23 17:36:20 -0700

    Time: 2 ms (95.19%) | Memory: 19.4 MB (33.05%) - LeetSync

--- 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.py
@@ -2,20 +2,23 @@ class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         target = len(graph)-1
         res = []
-        
-        def bt(node, path):
-            if node == target:
+        visited = [False] * len(graph)
+        def backtrack(cur, path):
+            if cur == target:
                 res.append(path.copy())
                 return 
+            visited[cur] = True
 
-            for nxt in graph[node]:
-                path.append(nxt)
-                bt(nxt, path)
-                path.pop()
+            for nei in graph[cur]:
+                if visited[nei] == False:
+                    path.append(nei)
+                    backtrack(nei, path)
+                    visited[nei] = False
+                    path.pop()
+            return 
+        backtrack(0, [0])
+        return res
 
-        bt(0, [0])
 
-        return res 
-            
 
 
