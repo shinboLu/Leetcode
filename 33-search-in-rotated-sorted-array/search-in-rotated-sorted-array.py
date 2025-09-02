ec847bcbd8c363469a8a78aabe7ab4e4ef22e3fa
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-09-01 18:49:38 -0700

    Time: 0 ms (100.00%) | Memory: 18 MB (66.58%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,24 +1,21 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-
-        left = 0 
+        left = 0
         right = len(nums)-1
-        # find pivot
-        while left <= right:
-            mid = (left+right)//2 
 
+        while left <= right:
+            mid = (left+right)//2
             if nums[mid] == target:
                 return mid
 
-            elif nums[mid] >= nums[left]:
+            if nums[mid] >= nums[left]:
                 if target >= nums[left] and target < nums[mid]:
-                    right = mid -1 
+                    right = mid -1
                 else:
-                    left = mid+1
+                    left = mid + 1
             else:
                 if target <= nums[right] and target > nums[mid]:
-                    left = mid + 1 
+                    left = mid + 1
                 else:
-                    right = mid-1 
-        return -1 
-
+                    right = mid -1
+        return -1
