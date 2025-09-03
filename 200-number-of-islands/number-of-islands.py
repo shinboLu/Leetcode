f4f9da7773047bf80beb875a9285fa529d857754
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-09-02 21:33:29 -0700

    Time: 244 ms (62.28%) | Memory: 20.1 MB (82.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,40 +1,24 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        dirs = [[0, 1], [1, 0], [-1,0], [0, -1]]
-        nrow = len(grid)
-        ncol = len(grid[0]) 
-        visited = set()
-        
-        def get_nei(x,y):
-            neigh = []
-            for dx, dy in dirs:
-                nx, ny = dx+x, dy+y
-                if 0<=nx<nrow and 0 <= ny < ncol:
-                    neigh.append((nx, ny)) 
 
-            return neigh
+        def dfs(x, y):
+            if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
+                return 
+            if grid[x][y] != '1':
+                return 
+            grid[x][y] = '0'
 
-        def bfs(x, y):
-            queue = collections.deque() 
-            queue.append((x, y))
-
-            
-            while queue:
-                cur_x, cur_y = queue.popleft()
-                for nx, ny in get_nei(cur_x, cur_y):
-                    if (nx,ny) not in visited and grid[nx][ny] == "1":
-                        visited.add((nx, ny))
-                        queue.append((nx, ny))
+            dfs(x+1, y)
+            dfs(x-1, y)
+            dfs(x, y-1)
+            dfs(x, y+1)
         res = 0
-        for row in range(nrow):
-            for col in range(ncol):
-                if grid[row][col] == '1' and (row, col) not in visited:
-                    bfs(row, col)
-                    res += 1
-        return res  
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col]=='1':
+                    res+=1
+                    dfs(row, col)
+        return res
 
 
 
-
-
-        
