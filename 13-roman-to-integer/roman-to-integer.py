c45ee5d1e732ef346213349871dd4fbf4c19b2bc
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-09-05 22:36:26 -0700

    Time: 3 ms (78.78%) | Memory: 17.8 MB (64.32%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,14 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dic = {'I':1, 'V':5, 'X': 10, 'L':50, 'C':100, 'D': 500, 'M':1000}
-        idx = 0
-        res = 0
+        symbol = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000,
+        }
 
-        while idx < len(s):
-            if idx+1 < len(s) and dic[s[idx]] < dic[s[idx+1]]:
-                res += dic[s[idx+1]] - dic[s[idx]]
-                idx += 2
-            else:
-                res += dic[s[idx]]
-                idx+=1
+        start = 0 
+        res = 0 
+        while start < len(s):
+            if start < len(s)-1 and symbol[s[start]] < symbol[s[start+1]]:
+                res += symbol[s[start+1]]-symbol[s[start]]
+                start+=2
+            else: 
+                res += symbol[s[start]]
+                start += 1
         return res
