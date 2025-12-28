403e5e89db54789525c55f029f056748f27877cf
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-12-28 15:30:49 -0800

    Time: 68 ms (50.57%) | Memory: 21.1 MB (16.44%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.py
+++ 971-shortest-bridge/shortest-bridge.py
@@ -1,49 +1,40 @@
 class Solution:
     def shortestBridge(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        dirs = [[1,0], [0,1], [-1,0], [0,-1]]
-        visited =set()
-        queue = collections.deque()
-        def dfs_find_island(x, y):
+        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
+        island_a = collections.deque()
+        visited = set()
+        distance = 0
+        def dfs_get_landA(x, y):
+            island_a.append((x,y))
             visited.add((x, y))
-            queue.append((x, y))
             for dx, dy in dirs:
                 nx, ny = dx+x, dy+y
-                if 0<=nx<n and 0 <= ny < n and grid[nx][ny] == 1 and (nx, ny) not in visited:
-                    dfs_find_island(nx, ny)
-        
-        
-        first_x = 0
-        first_y = 0
+                if 0<=nx<n and 0<=ny<n and (nx, ny) not in visited and grid[nx][ny]==1:
+                    dfs_get_landA(nx, ny)
+        start_x, start_y = 0, 0
         for row in range(n):
             for col in range(n):
                 if grid[row][col] == 1:
-                    first_x = row
-                    first_y = col
+                    start_x, start_y = row, col
                     break
-        dfs_find_island(first_x, first_y)
+        dfs_get_landA(start_x, start_y)
+        
 
-        distance = 0
-        while queue:
-            for _ in range(len(queue)):
-            # new_bfs = collections.deque()
-                cur_x, cur_y = queue.popleft()
-                for dx, dy in dirs:
-                    nx, ny = dx+cur_x, dy+cur_y
-                    if 0<=nx<n and 0<=ny<n and (nx,ny) not in visited: 
-                        if grid[nx][ny] == 1:
-                            print(f"Found second island at ({nx}, {ny}), distance={distance}")
-                            return distance
-                        visited.add((nx, ny))
-                        queue.append((nx, ny))
-                        # elif grid[nx][ny] == 0:
-                            # new_bfs.append((nx, ny))
-                            # visited.add((nx, ny))
-            distance += 1
-            # queue = new_bfs
-        return distance
+        def bfs(queue):
+            nonlocal distance
+            while queue:
+                for _ in range(len(queue)):
+                    cx, cy = queue.popleft()
+                    for dx, dy in dirs:
+                        nx, ny = dx+cx, dy+cy
+                        if 0<=nx<n and 0<=ny<n and (nx, ny) not in visited:
+                            if grid[nx][ny] == 1: 
+                                return distance
+                            else:
+                                visited.add((nx, ny))
+                                queue.append((nx, ny))
+                distance +=1
 
-
-
-
+        return bfs(island_a)
 
