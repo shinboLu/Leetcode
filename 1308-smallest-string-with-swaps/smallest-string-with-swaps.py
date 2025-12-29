7b73d3e1939ff7abdfc8fee4eaa94bdb372e4a04
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-12-28 22:01:36 -0800

    Time: 96 ms (88.37%) | Memory: 46.5 MB (59.28%) - LeetSync

--- 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
+++ 1308-smallest-string-with-swaps/smallest-string-with-swaps.py
@@ -1,47 +1,49 @@
 class UnionFind:
-    def __init__(self, size):
-        self.root = [x for x in range(size)]
-        self.rank = [1 for _ in range(size)]
-    
+    def __init__(self, size ):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
     def find(self, x):
         if self.root[x] == x:
-            return x
+            return x 
         self.root[x] = self.find(self.root[x])
-        return self.root[x] 
+        return self.root[x]
+
     def union(self, x, y):
         root_x = self.find(x)
         root_y = self.find(y)
-
-        if self.rank[root_x] > self.rank[root_y]:
-            self.root[root_y] = root_x
-        elif self.rank[root_x] < self.rank[root_y]:
-            self.root[root_x] = root_y
-        else:
-            self.root[root_y] = root_x
-            self.rank[root_x] +=1
-    def isConnected(self,x,y):
+        if root_x != root_y:
+            if self.rank[root_x] > self.rank[root_y]:
+                self.root[root_y] = root_x
+            elif self.rank[root_y] > self.rank[root_x]:
+                self.root[root_x] = root_y
+            else:
+                self.root[root_y] = root_x
+                self.rank[root_x] += 1
+    def is_connected(self, x, y):
         return self.find(x) == self.find(y)
-        
-class Solution: 
+
+class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        n = len(s) 
+        n = len(s)
         uf = UnionFind(n)
-        s_li = [char for char in s]
-        res = float(inf)
 
-        for idx1, idx2 in pairs:
-            uf.union(idx1, idx2)
-        
-        group = collections.defaultdict(lambda: ([], []))
+        for x, y in pairs:
+            uf.union(x, y)
 
+        # 2. Grouping
+        group = defaultdict(lambda: ([], []))
         for i, ch in enumerate(s):
             parent = uf.find(i)
             group[parent][0].append(i)
-            group[parent][1].append(ch) 
-        res = [''] * n
-        for idx, chars in group.values():
-            idx.sort()
+            group[parent][1].append(ch)
+
+        # 3. Sorting
+        res = [''] * len(s)
+        for ids, chars in group.values():
+            ids.sort()
             chars.sort()
-            for ch, i in zip(chars,idx):
+            for ch, i in zip(chars, ids):
                 res[i] = ch
-        return "".join(res)
+
+        return ''.join(res)
