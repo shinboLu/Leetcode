858e235fe68743d33e748102c80350dfe28428e8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-12-31 09:06:29 -0800

    Time: 116 ms (9.45%) | Memory: 27.8 MB (85.30%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -5,13 +5,14 @@ class Solution:
         res = float('-inf')
 
         while left < right:
-            h1 = min(height[left], height[right])
-            w1 = right-left
-            cur_area = h1*w1
+            cur_wid = right-left
+            cur_hei = min(height[left], height[right])
+            area = cur_wid * cur_hei
+            res = max(res, area)
             if height[left] < height[right]:
-                left +=1
+                left+=1
             else:
-                right -= 1
-            res = max(cur_area, res)
-
+                right-=1
+            
         return res
+                
