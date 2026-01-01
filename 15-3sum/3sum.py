302c5e5fe957f8bd0fcb9c2656bf007b4399b97c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2025-12-31 17:17:21 -0800

    Time: 739 ms (26.54%) | Memory: 19.5 MB (99.44%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,28 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort() 
+        nums.sort()
         res = []
-        def twoSum(i, res):
-            left, right = i+1, len(nums)-1
-            while left < right:
-                cur_sum = nums[i] + nums[left] + nums[right]
-                if cur_sum < 0:
-                    left += 1
-                elif cur_sum > 0:
-                    right -= 1
-                else:
-                    res.append((nums[i], nums[left], nums[right]))
-                    left += 1
-                    right -= 1
+        def two_sum(i):
+            left = i+1
+            right = len(nums)-1
 
+            while left < right:
+                if nums[i]+nums[left]+nums[right]==0:
+                    res.append((nums[i], nums[left], nums[right]))
+                    left+=1
+                    right-=1
                     while left < right and nums[left] == nums[left-1]:
                         left += 1
-
+                    while left < right and nums[right]==nums[right+1]:
+                        right -=1
+                elif nums[i]+nums[left]+nums[right] < 0:
+                    left += 1
+                else:
+                    right-=1
         for i in range(len(nums)):
             if nums[i] > 0:
-                break  
-            if i == 0 or nums[i-1] != nums[i]:
-                twoSum(i, res)
-        return res 
-
+                continue
+            if i == 0 or nums[i] != nums[i-1]:
+                two_sum(i)
+        return res
 
