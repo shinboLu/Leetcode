8407c8e8f016629c1616b0a6a24392b36d42f088
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-02 21:01:26 -0800

    Time: 6 ms (50.05%) | Memory: 18.7 MB (84.71%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -2,28 +2,26 @@ class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         adj_list = collections.defaultdict(list)
 
-        for c,p_c in prerequisites:
-            adj_list[p_c].append(c)
-
+        for a, b in prerequisites:
+            adj_list[b].append(a)
+        
         visited = [False] * numCourses
-        in_stack = [False] * numCourses
+        instack = [False] * numCourses
 
-        def dfs(cur_c, visited, in_stack):
-            if in_stack[cur_c]:
-                return False
-
-            if visited[cur_c]:
+        def dfs(course):
+            if instack[course]:
                 return True
-            
-            in_stack[cur_c] = True
-            visited[cur_c] = True
-            for n_c in adj_list[cur_c]:
-                if not dfs(n_c, visited, in_stack):
-                    return False
-            in_stack[cur_c] = False
-            return True
+            if visited[course]:
+                return False
+            visited[course] = True
+            instack[course] = True
+            for nei in adj_list[course]:
+                if dfs(nei):
+                    return True
+            instack[course] = False
+            return False
 
         for i in range(numCourses):
-            if not dfs(i, visited, in_stack):
+            if dfs(i):
                 return False
         return True
