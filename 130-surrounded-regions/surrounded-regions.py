93da594093477b6a7f910b3ac68ee5d82e7e1c43
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-04 21:38:48 -0800

    Time: 6 ms (61.08%) | Memory: 20.8 MB (89.49%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,28 +3,35 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
+        dirs = [[0,1],[1,0],[-1,0],[0,-1]]
         nrow = len(board)
-        ncol = len(board[0]) 
-
+        ncol = len(board[0])
 
         def dfs(x, y):
-            if x < 0 or x >= nrow or y < 0 or y >= ncol or board[x][y] != 'O':
-                return 
-            board[x][y] = 'T'
-            for dx, dy in dirs:
-                dfs(dx+x, dy+y)
-        
-        for x in range(nrow):
-            for y in [0, ncol-1]:
-                dfs(x, y)
-        for x in range(ncol):
-            for y in [0, nrow-1]:
-                dfs(y, x)
+            if x<0 or x >= nrow or y < 0 or y >=ncol or board[x][y]!='O':
+                return
+            board[x][y] = 'V'
 
-        for x in range(nrow):
-            for y in range(ncol):
-                if board[x][y] == 'O':
-                    board[x][y] = 'X'
-                elif board[x][y] == 'T':
-                    board[x][y] = 'O'
+            for dx, dy in dirs:
+                nx, ny = dx+x, dy+y
+                dfs(nx, ny)
+
+        for row in range(nrow):
+            for col in [0, ncol-1]:
+                dfs(row, col)
+
+        for col in range(ncol):
+            for row in [0, nrow-1]:
+                dfs(row, col)
+        print(board)
+
+        for row in range(nrow):
+            for col in range(ncol):
+                if board[row][col] == 'O':
+                    board[row][col] = 'X'
+                elif board[row][col] =='V':
+                    board[row][col] = 'O'
+
+
+
+
