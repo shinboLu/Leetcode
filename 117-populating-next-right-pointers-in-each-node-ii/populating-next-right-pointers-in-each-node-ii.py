a101f4cf1c313f73197b99ac2338a8e773a0687f
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-04 16:36:24 -0800

    Time: 48 ms (40.20%) | Memory: 18.4 MB (84.41%) - LeetSync

--- 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
+++ 117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.py
@@ -10,22 +10,28 @@ class Node:
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
-        if not root:
-            return None
-        queue = collections.deque()
-
-        queue.append(root)
-
-        while queue:
-            n = len(queue)
-            pre_node = None
-            for _ in range(n):
-                cur=queue.popleft()
-                if pre_node:
-                    pre_node.next = cur
-                pre_node = cur
+        def get_child(node):
+            cur = node
+            while cur:
                 if cur.left:
-                    queue.append(cur.left)
+                    return cur.left
                 if cur.right:
-                    queue.append(cur.right) 
+                    return cur.right
+                cur = cur.next
+            return None
+
+        def dfs(node):
+            if not node:
+                return
+            if node.left:
+                if node.right:
+                    node.left.next = node.right
+                else:
+                    node.left.next = get_child(node.next)
+            if node.right:
+                node.right.next = get_child(node.next)
+
+            dfs(node.right)
+            dfs(node.left) 
+        dfs(root)
         return root
