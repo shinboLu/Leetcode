7520390c428d63ebe121df62cd4435bd220e25dc
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-06 15:43:53 -0800

    Time: 0 ms (100.00%) | Memory: 19.6 MB (17.40%) - LeetSync

--- 364-nested-list-weight-sum-ii/nested-list-weight-sum-ii.py
+++ 364-nested-list-weight-sum-ii/nested-list-weight-sum-ii.py
@@ -43,25 +43,27 @@
 
 class Solution:
     def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
-        max_depth=float('-inf')
-        res = 0 
-        def dfs_get_max_depth(nest_list, cur_depth):
+        max_depth = float('-inf')
+        res = 0
+        def get_max_depth(nlist, cur_depth):
             nonlocal max_depth
-            for sub_list in nest_list:
-                if sub_list.isInteger():
+            for li in nlist:
+                if li.isInteger():
                     max_depth = max(max_depth, cur_depth)
                 else:
-                    dfs_get_max_depth(sub_list.getList(), cur_depth+1)
-        dfs_get_max_depth(nestedList, 1)
+                    get_max_depth(li.getList(), cur_depth+1)
+        get_max_depth(nestedList, 1)
 
-        def dfs_get_sum(nest_list, cur_depth):
-            nonlocal max_depth, res
-            for sub_list in nest_list:
-                if sub_list.isInteger():
-                    res+=sub_list.getInteger()* (max_depth-cur_depth+1)
+        def dfs(nlist, cur_sum, cur_depth):
+            nonlocal res, max_depth
+            for li in nlist:
+                if li.isInteger():
+                    cur_sum = li.getInteger() * (max_depth-cur_depth+1)
+                    res+=cur_sum
                 else:
-                    dfs_get_sum(sub_list.getList(), cur_depth+1)
+                    dfs(li.getList(), cur_sum, cur_depth+1)
+        dfs(nestedList, 0, 1)
 
-        dfs_get_sum(nestedList, 1)
         return res
 
+        
