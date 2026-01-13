57c9f90b2cc6182b7e47ae9db49fe0c842879ef8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-12 22:51:01 -0800

    Time: 3 ms (37.08%) | Memory: 19.5 MB (7.37%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -1,23 +1,21 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) < 2:
-            return False
         pairs = {
-            "}":"{",
-            "]":"[",
-            ")": "("
-            }
-
+            '}': '{',
+            ')': '(',
+            ']':'['
+        }
         stack = [] 
 
-        for char in s:
-            if char not in pairs:
-                stack.append(char)
+        for char in s: 
+            if char in pairs.values():
+                stack.append(char) 
+            elif not stack:
+                return False
+            elif pairs[char] == stack[-1]:
+                stack.pop()
             else:
-                if stack and stack[-1] == pairs[char]:
-                    stack.pop()
-                else:
-                    return False
-        return True if not stack else False
+                return False
+            
+        return len(stack)==0
 
-                    
