955a903aa398298640723baf1e4070e139bacf12
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-17 21:29:04 -0800

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.99%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,32 +1,28 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
-            return [] 
-        graph = {
-            '2': ['a', 'b', 'c'],
-            '3' : ['d', 'e', 'f'],
-            '4' : ['g', 'h', 'i'],
-            '5' : ['j','k','l'],
-            '6' : ['m','n','o'],
-            '7' : ['p','q','r', 's'],
-            '8': ['t', 'u', 'v'],
-            '9': ['w', 'x', 'y', 'z']
+        mapping = {
+            '2':'abc',
+            '3':'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7':'pqrs',
+            '8': 'tuv',
+            '9':'wxyz'
         }
 
-        res = []
+        res = [] 
 
         def bt(idx, combs):
+            nonlocal res
             if idx == len(digits):
-                res.append(''.join(combs))
+                res.append(''.join(combs.copy()))
                 return 
 
-            cur_num = digits[idx]
-
-            for d in graph[cur_num]:
-                combs.append(d)
+            for char in mapping[digits[idx]]:
+                combs.append(char)
                 bt(idx+1, combs)
                 combs.pop()
-        bt(0, [])
 
-        return res 
-            
+        bt(0, [])
+        return res
