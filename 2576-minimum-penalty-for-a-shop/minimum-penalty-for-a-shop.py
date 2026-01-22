0501e14672925c5b279acf0de889c71b0b0f34a6
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-01-21 22:04:53 -0800

    Time: 47 ms (75.66%) | Memory: 19.9 MB (26.77%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.py
@@ -1,17 +1,22 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         max_penalty = customers.count('Y')
+        
         cur_penalty = max_penalty
         res = 0
-        
-        for idx, val in enumerate(customers):
-            if val == 'Y':
-                cur_penalty-=1
+
+        for idx, c in enumerate(customers):
+            if c == 'Y':
+                cur_penalty -=1
             else:
-                cur_penalty +=1
+                cur_penalty += 1
             
             if cur_penalty < max_penalty:
                 max_penalty = cur_penalty
-                res = idx+1
+                res = idx + 1
+            
         return res
+
+            
+
 
