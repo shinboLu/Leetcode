59c4c6a9d0a642fd42de1749824e44697ab07ac4
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-16 21:24:16 -0800

    Time: 5 ms (55.45%) | Memory: 20.6 MB (42.26%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,27 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        adj_list = collections.defaultdict(list)
+        adj_list = collections.defaultdict(list) 
+        indegrees = [0] * numCourses
 
         for a, b in prerequisites:
             adj_list[b].append(a)
-        
-        visited = [False] * numCourses
-        instack = [False] * numCourses
+            indegrees[a] +=1
+        queue = collections.deque()
+        for idx, val in enumerate(indegrees):
+            if val == 0:
+                queue.append(idx)
+        visited = set()
+        while queue:
+            cur_c = queue.popleft()
+            visited.add(cur_c)
 
-        def dfs(course):
-            if instack[course]:
-                return True
-            if visited[course]:
-                return False
-            visited[course] = True
-            instack[course] = True
-            for nei in adj_list[course]:
-                if dfs(nei):
-                    return True
-            instack[course] = False
-            return False
+            for nei in adj_list[cur_c]:
+                indegrees[nei] -=1
+                if indegrees[nei] == 0:
+                    queue.append(nei)
+        return len(visited) == numCourses
 
-        for i in range(numCourses):
-            if dfs(i):
-                return False
-        return True
+
+
