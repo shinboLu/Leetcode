c4a1824c9a475625202c702335ef96729564ee1c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-21 16:37:04 -0800

    Time: 7 ms (77.41%) | Memory: 19.8 MB (71.46%) - LeetSync

--- 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -1,35 +1,29 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        # SORT THE WEBSITE INFO BASED ON THE TIMESTAMPS
-        webInfo = []
-        for time, usr, web in zip(timestamp, username, website):
-            webInfo.append((time, usr, web))
-            
-        webInfo.sort(key=lambda x:x[0])
-        # print(webInfo)
+        _zip = []
+        for time, user, web in zip(timestamp, username, website):
+            _zip.append([time, user, web])
+        _zip.sort(key = lambda x: x[0])
+
+        user_website_map = collections.defaultdict(list)
+
+        for _, user, web in _zip:
+            user_website_map[user].append(web)
+
+        scores = collections.defaultdict(int)
+
+        for user in user_website_map:
+            routes = set(combinations(user_website_map[user],3))
+            for route in routes:
+                scores[route] += 1
+        max_val = max(scores.values())
+        res = []
+        for route, score in scores.items():
+            if score == max_val:
+                res.append(route)
+        if len(route) > 1:
+            res.sort()
+
+        return res[0]
+
         
-        # FIND THE WEBSITES VISITED BY PARTICULAR USERS
-        websiteVisit = defaultdict(list)
-        for _, usr, web in webInfo:
-            websiteVisit[usr].append(web)
-            
-        # print(websiteVisit)
-        # FIND THE ROUTES IN THE FORM OF TUPLES OF LENGTH 3
-        possibleTuples = defaultdict(int)
-        for usr in websiteVisit:
-            webRoutes = set(combinations(websiteVisit[usr], 3))
-            for webRoute in webRoutes:
-                possibleTuples[webRoute] += 1
-                
-        # print(possibleTuples)
-        # FIND MAX VALUE OF USERS VISITED
-        maxVal, routes = max(possibleTuples.values()), []
-        for r, val in possibleTuples.items():
-            if val == maxVal:
-                routes.append(r)
-                
-        if len(routes) > 1:
-            # SORTS LEXICOGRAPHICALLY
-            routes.sort()
-        
-        return routes[0]
