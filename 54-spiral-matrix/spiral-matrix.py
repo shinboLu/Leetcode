54abc0fc7d74fa3d7bebbcf9f66be2ad4d3926ec
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-22 17:28:24 -0800

    Time: 0 ms (100.00%) | Memory: 19.5 MB (30.11%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,36 +1,36 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        nrow = len(matrix) 
+        nrow = len(matrix)
         ncol = len(matrix[0])
-        res = []
 
-        dirs = [[0,1], [1, 0], [0, -1], [-1, 0]] 
-
-        steps = nrow * ncol
         start_r = 0 
         start_c = 0 
-        end_c = ncol-1
-        end_r = nrow-1
 
-        while len(res) < nrow*ncol:
+        end_r = nrow-1
+        end_c = ncol-1
+
+        res = [] 
+
+        while len(res) < nrow * ncol:
+            ## go right
             for col in range(start_c, end_c+1):
                 res.append(matrix[start_r][col])
-            
+
+            ## go down
             for row in range(start_r+1, end_r+1):
                 res.append(matrix[row][end_c])
             
-            if start_r!= end_r:
+            ## go left (bottom) if more than one row
+            if start_r != end_r:
                 for col in range(end_c-1, start_c-1, -1):
                     res.append(matrix[end_r][col])
-            if start_c != end_c:
+            ## go up if more than one column
+            if start_c != end_c: 
                 for row in range(end_r-1, start_r, -1):
                     res.append(matrix[row][start_c])
             start_r+=1
-            end_r-=1
             start_c+=1
+            end_r-=1
             end_c-=1
         return res
-
-            
-
 
