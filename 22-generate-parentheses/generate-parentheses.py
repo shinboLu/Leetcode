3f630a56e0aa3dd1de76b222f14c3ad45a8b75ff
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-24 22:19:39 -0800

    Time: 1 ms (42.64%) | Memory: 19.4 MB (79.41%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -1,21 +1,22 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        res = []
+        res = [] 
+
         def bt(left, right, combs):
             if len(combs) == n*2:
-                res.append(''.join(combs.copy()))
-                return
-
+                res.append(''.join(combs))
+                return 
+            
             if left < n:
                 combs.append('(')
                 bt(left+1, right, combs)
                 combs.pop()
+            
             if right < left:
                 combs.append(')')
                 bt(left, right+1, combs)
                 combs.pop()
-            
+        
+        bt(0, 0, [])
 
-        bt(0,0,[])
-
-        return res            
+        return res
