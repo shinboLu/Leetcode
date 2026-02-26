6f47cc5e8a722607ad7281dca870bc76b6d0afc3
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-25 22:48:53 -0800

    Time: 3 ms (25.37%) | Memory: 19.3 MB (73.13%) - LeetSync

--- 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.py
+++ 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.py
@@ -1,10 +1,14 @@
-import bisect
 class Solution:
-    def minimizeSet(self, d1: int, d2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
-        lcm = math.lcm(d1, d2)
-        def check(x: int) -> bool:
-            left1 = max(uniqueCnt1 - x // d2 + x // lcm, 0)
-            left2 = max(uniqueCnt2 - x // d1 + x // lcm, 0)
-            common = x - x // d1 - x // d2 + x // lcm
-            return common >= left1 + left2
-        return bisect.bisect_left(range((uniqueCnt1 + uniqueCnt2) * 2 - 1), True, key=check)
+    def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
+        left = 1 
+        right = 1 * 10**20
+        multiplier = math.lcm(divisor1, divisor2) 
+        while left < right:
+            mid = (left+right)//2 
+            if uniqueCnt1 <= mid - mid // divisor1 and uniqueCnt2 <= mid - mid // divisor2 \
+            and uniqueCnt1 + uniqueCnt2 <= mid - mid // multiplier:
+                right = mid 
+            else:
+                left = mid + 1
+
+        return left
