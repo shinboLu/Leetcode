2148e53f41f0a5975dbc03f5e299c1bee34ce8a8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-27 19:44:16 -0800

    Time: 0 ms (100.00%) | Memory: 20.6 MB (48.34%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,31 +1,26 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1,-1]
-        left = self.findLeft(nums, target)
-        right = self.findRight(nums, target)
+        def get_left_idx(left, right):
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] >= target:
+                    right = mid -1
+                else:
+                    left = mid + 1
+            return left
 
-        return [left, right]
-    def findLeft(self, nums, target):
-        left, right= 0, len(nums)-1
-        while left <= right:
-            mid = (left+right)//2
-            if nums[mid] < target:
-                left = mid + 1
-            else:
-                right = mid -1
-        print(right+1)
-        return right+1 if right+1 >= 0 and right+1 <= len(nums)-1 and nums[right+1] == target else -1
-
-    def findRight(self, nums, target):
-        left, right = 0, len(nums)-1
-
-        while left <= right:
-            mid = (left+right)//2
-            if nums[mid] <= target:
-                left = mid + 1
-            else:
-                right = mid -1 
-
-        return right if right >= 0 and nums[right] == target else -1
-
+        def get_right_idx(left, right):
+            while left <= right:
+                mid = (left+right)//2
+                if nums[mid] <= target:
+                    left = mid + 1 
+                else:
+                    right = mid -1
+            return right
+        
+        left_side = get_left_idx(0, len(nums)-1)
+        if left_side == len(nums) or nums[left_side] != target:
+            return [-1, -1]
+        right_side = get_right_idx(0, len(nums)-1)
+        
+        return [left_side, right_side]
