3720d7dc05fd930af6b072e97e193cc032216001
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-02-27 23:26:38 -0800

    Time: 0 ms (100.00%) | Memory: 20.5 MB (87.17%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,26 +1,27 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def get_left_idx(left, right):
+        def get_left(left, right):
             while left <= right:
                 mid = (left+right)//2
-                if nums[mid] >= target:
-                    right = mid -1
+                if nums[mid] < target:
+                    left = mid+1
                 else:
-                    left = mid + 1
+                    right = mid -1
             return left
 
-        def get_right_idx(left, right):
+        def get_right(left, right):
             while left <= right:
-                mid = (left+right)//2
+                mid = (left+right)//2  
                 if nums[mid] <= target:
-                    left = mid + 1 
+                    left = mid + 1
                 else:
                     right = mid -1
             return right
-        
-        left_side = get_left_idx(0, len(nums)-1)
-        if left_side == len(nums) or nums[left_side] != target:
+
+        left = get_left(0, len(nums)-1)
+        right = get_right(0, len(nums)-1)
+
+        if left == len(nums) or nums[left] != target:
             return [-1, -1]
-        right_side = get_right_idx(0, len(nums)-1)
-        
-        return [left_side, right_side]
+
+        return [left, right]
