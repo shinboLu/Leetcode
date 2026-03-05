5abb7c967654e4b4c7dbcedf10cf8937abf15ed8
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-04 23:32:14 -0800

    Time: 71 ms (24.49%) | Memory: 24.2 MB (87.09%) - LeetSync

--- 436-find-right-interval/find-right-interval.py
+++ 436-find-right-interval/find-right-interval.py
@@ -1,31 +1,26 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        orders = {val[0]: idx for idx, val in enumerate(intervals)}
-        sorted_start = sorted(orders.keys())
-        starts = []
+        start_tracker = []
+        for idx, intv in enumerate(intervals):
+            start_tracker.append((intv[0], idx))
+
+        start_tracker.sort(key = lambda x: x[0])
+
+        def binary_search(start_tracker, target):
+            left = 0 
+            right = len(start_tracker)-1
+            res = -1
+            while left <=right:
+                mid = (left+right)//2
+                if start_tracker[mid][0] >= target:
+                    res = start_tracker[mid][1]
+                    right = mid-1
+                else:
+                    left = mid+1
+            return res
         res = []
-        for i in range(len(intervals)):
-            starts.append(self.binarySearch(sorted_start, intervals[i][1]))
+        for s, e in intervals:
+            idx = binary_search(start_tracker, e)
+            res.append(idx)
 
-        for start_val in starts: 
-            if start_val is not None:
-                res.append(orders[start_val])
-            else:
-                res.append(-1)
         return res
-
-    def binarySearch(self,sorted_start, target):
-        left = 0
-        right = len(sorted_start)-1
-        while left <= right:
-            mid = (left+right)//2 
-
-            if sorted_start[mid] < target:
-                left = mid + 1 
-            else:
-                right = mid -1 
-
-        if right+1 < 0 or right + 1 >= len(sorted_start):
-            return None
-        else:
-            return sorted_start[right+1]
