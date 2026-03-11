15ebd83e4a6e0fbfb7cc0e881d7fbcd15faf6274
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-10 22:55:29 -0700

    Time: 43 ms (75.36%) | Memory: 43.6 MB (32.61%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.py
@@ -1,16 +1,17 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        max_li = collections.deque()
-        min_li = collections.deque()
-        res = []
+        neg = []
+        pos = [] 
+
         for num in nums:
-            if num >= 0:
-                max_li.append(num)
+            if num < 0:
+                neg.append(num)
             else:
-                min_li.append(num)
-        print(max_li, min_li)
-        while max_li and min_li:
-            res.append(max_li.popleft())
-            res.append(min_li.popleft())
+                pos.append(num)
+        res = [] 
+
+        for n, p in zip(neg, pos):
+            res.append(p)
+            res.append(n)
 
         return res
