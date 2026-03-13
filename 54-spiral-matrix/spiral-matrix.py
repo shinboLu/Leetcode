c1ff40454c23ecaaa8d8e452b621b36e6bbd1b30
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-13 00:11:18 -0700

    Time: 0 ms (100.00%) | Memory: 19.1 MB (98.53%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.py
+++ 54-spiral-matrix/spiral-matrix.py
@@ -1,36 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         nrow = len(matrix)
-        ncol = len(matrix[0])
+        ncol = len(matrix[0]) 
+        row_s = 0 
+        row_e = nrow-1
+        col_s = 0
+        col_e = ncol-1
 
-        start_r = 0 
-        start_c = 0 
-
-        end_r = nrow-1
-        end_c = ncol-1
-
-        res = [] 
+        res = []
 
         while len(res) < nrow * ncol:
-            ## go right
-            for col in range(start_c, end_c+1):
-                res.append(matrix[start_r][col])
-
-            ## go down
-            for row in range(start_r+1, end_r+1):
-                res.append(matrix[row][end_c])
+            for col in range(col_s, col_e+1): 
+                res.append(matrix[row_s][col])
             
-            ## go left (bottom) if more than one row
-            if start_r != end_r:
-                for col in range(end_c-1, start_c-1, -1):
-                    res.append(matrix[end_r][col])
-            ## go up if more than one column
-            if start_c != end_c: 
-                for row in range(end_r-1, start_r, -1):
-                    res.append(matrix[row][start_c])
-            start_r+=1
-            start_c+=1
-            end_r-=1
-            end_c-=1
-        return res
+            for row in range(row_s+1, row_e+1):
+                res.append(matrix[row][col_e])
+            
+            if row_s != row_e:
+                for col in range(col_e-1, col_s-1, -1):
+                    res.append(matrix[row_e][col])
 
+            if col_s != col_e:
+                for row in range(row_e-1, row_s, -1):
+                    res.append(matrix[row][col_s])
+            row_s+=1
+            row_e-=1
+            col_s+=1
+            col_e-=1 
+        return res
