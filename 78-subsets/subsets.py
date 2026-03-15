f1cb6a8e84283467872b6543ee1efa1025af1022
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-15 07:43:41 -0700

    Time: 0 ms (100.00%) | Memory: 19.4 MB (60.56%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,18 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ## unique
-
         res = []
-        
-        def bt(index, comb):
-            res.append(comb.copy())
-
-            for i in range(index, len(nums)):
-                comb.append(nums[i])
-                bt(i+1, comb)
-                comb.pop()
+        def bt(idx, comb):
+            if idx == len(nums):
+                res.append(comb.copy())
+                return
+            
+            if idx > len(nums):
+                return 
+            
+            comb.append(nums[idx])
+            bt(idx+1, comb)
+            comb.pop()
+            bt(idx+1, comb)
 
         bt(0, [])
         return res
 
-            
+
