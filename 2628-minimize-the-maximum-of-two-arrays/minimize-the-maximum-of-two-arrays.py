5bcc682a975dd3bb257b3e730279077bd75da5ff
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-15 20:17:44 -0700

    Time: 0 ms (100.00%) | Memory: 19.5 MB (58.21%) - LeetSync

--- 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.py
+++ 2628-minimize-the-maximum-of-two-arrays/minimize-the-maximum-of-two-arrays.py
@@ -1,14 +1,14 @@
 class Solution:
     def minimizeSet(self, divisor1: int, divisor2: int, uniqueCnt1: int, uniqueCnt2: int) -> int:
-        left = 1 
-        right = 1 * 10**20
-        multiplier = math.lcm(divisor1, divisor2) 
-        while left < right:
-            mid = (left+right)//2 
-            if uniqueCnt1 <= mid - mid // divisor1 and uniqueCnt2 <= mid - mid // divisor2 \
-            and uniqueCnt1 + uniqueCnt2 <= mid - mid // multiplier:
-                right = mid 
-            else:
-                left = mid + 1
+        lcm = math.lcm(divisor1, divisor2) 
 
+        left = 1
+        right = 10 ** 31
+
+        while left <= right:
+            mid = (left+right) //2 
+            if uniqueCnt1 <= mid - mid//divisor1 and uniqueCnt2 <= mid-mid//divisor2 and uniqueCnt1+uniqueCnt2 <= mid - mid // lcm:
+                right = mid -1
+            else:
+                left = mid+1
         return left
