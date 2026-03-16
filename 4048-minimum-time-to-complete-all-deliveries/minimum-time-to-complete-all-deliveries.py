b1e6117f550fb7b35e6dabaa59f573eeabc63505
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-15 21:14:56 -0700

    Time: 19 ms (16.58%) | Memory: 19.3 MB (80.69%) - LeetSync

--- 4048-minimum-time-to-complete-all-deliveries/minimum-time-to-complete-all-deliveries.py
+++ 4048-minimum-time-to-complete-all-deliveries/minimum-time-to-complete-all-deliveries.py
@@ -1,15 +1,19 @@
 class Solution:
     def minimumTime(self, d: List[int], r: List[int]) -> int:
-        left = 0 
+        left = 1
         right = 10 ** 18 
         multi = lcm(r[0], r[1])
 
-        while left < right:
-            mid = (left+right)//2 
+        while left <= right:
+            ## mid is the total time in hours to complete all delivery
+            mid = (left+right) //2 
 
-            if d[0] <= mid - mid // r[0] and d[1] <= mid - mid // r[1] and sum(d) <= mid - mid//multi:
-                right = mid
+            if d[0] <= mid - mid//r[0] and d[1] <= mid - mid//r[1] and d[0] + d[1] <= mid - mid//multi:
+                right = mid -1
             else:
-                left = mid + 1
+                left = mid +1 
 
         return left
+            
+
+        
