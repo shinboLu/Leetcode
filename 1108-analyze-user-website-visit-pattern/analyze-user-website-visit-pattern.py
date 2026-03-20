5c6df38fdf8e93af3db7314a9d97565672b8be58
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-19 21:02:29 -0700

    Time: 3 ms (98.51%) | Memory: 20.1 MB (41.08%) - LeetSync

--- 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
+++ 1108-analyze-user-website-visit-pattern/analyze-user-website-visit-pattern.py
@@ -1,28 +1,30 @@
 class Solution:
     def mostVisitedPattern(self, username: List[str], timestamp: List[int], website: List[str]) -> List[str]:
-        combs = []
-        for user, time, web in zip(username, timestamp, website):
-            combs.append([time, user, web])
-        combs.sort(key = lambda x:x[0])
 
-        user_web = collections.defaultdict(list)
-        for _, user, web in combs:
-            user_web[user].append(web)
-        print(user_web)
+        utw_list = []
+        for time, user, web in zip(timestamp, username, website):
+            utw_list.append([time, user, web])
+        utw_list.sort(key = lambda x: x[0])
+        
+        uw_map = collections.defaultdict(list)
+
+        for _, user, web in utw_list:
+            uw_map[user].append(web)
+
         scores = collections.defaultdict(int)
 
-        for user in user_web:
-            choices = set(combinations(user_web[user], 3))
-            for choice in choices:
-                scores[choice] +=1
-        
+        for user in uw_map:
+            combs = set(combinations(uw_map[user], 3))
+            for comb in combs:
+                scores[comb] +=1
+
         max_val = max(scores.values())
         res = []
-        for route, score in scores.items():
-            if score == max_val:
-                res.append(route)
-        
+
+        for comb, count in scores.items():
+            if count == max_val:
+                res.append(comb) 
+
         res.sort()
         return res[0]
-            
             
