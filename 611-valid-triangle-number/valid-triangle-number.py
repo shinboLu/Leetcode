63321e9e651f822d87384c2508d34a0a02b23714
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-03-21 20:14:34 -0700

    Time: 437 ms (94.86%) | Memory: 19.4 MB (32.46%) - LeetSync

--- 611-valid-triangle-number/valid-triangle-number.py
+++ 611-valid-triangle-number/valid-triangle-number.py
@@ -3,13 +3,14 @@ class Solution:
         nums.sort()
         res = 0
         for i in range(len(nums)):
-            left = 0
+            left = 0 
             right = i-1
+
             while left < right:
                 if nums[left] + nums[right] > nums[i]:
                     res += right-left
-                    right-=1
+                    right-=1 
                 else:
-                    left +=1
+                    left +=1 
 
         return res
