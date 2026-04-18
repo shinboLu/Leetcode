4ce9d593404b27172f73a2ff07dcce64d59de9f3
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-04-18 16:32:15 -0700

    Time: 7 ms (79.90%) | Memory: 22.3 MB (56.49%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,13 +1,13 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        products.sort() 
-        res = []
+        products.sort()
         cur = ''
-        cur_idx = 0
-        for c in searchWord:
-            cur+=c
-            insert_idx = bisect.bisect_left(products, cur, cur_idx)
-            res.append([w for w in products[insert_idx:insert_idx+3] if w.startswith(cur)])
+        res = []
+        cur_idx = 0 
+        for char in searchWord:
+            cur += char
+            idx = bisect.bisect_left(products, cur, cur_idx)
+            res.append([w for w in products[idx:idx+3] if w.startswith(cur)]) 
+            cur_idx = idx
 
         return res
-
