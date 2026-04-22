073c091ec761e2d0f02fe869a88c15197b7f2618
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-04-21 21:24:10 -0700

    Time: 2 ms (77.57%) | Memory: 26.7 MB (28.63%) - LeetSync

--- 189-rotate-array/rotate-array.py
+++ 189-rotate-array/rotate-array.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start +=1
+                end -=1
         n = len(nums)
-        # steps = n - (k % n)
-        steps = k % n
-        print(steps)
-        while steps > 0:
-            first = nums.pop(-1)
-            nums.insert(0, first)
-            steps -=1
+        k = k %n
+        nums.reverse()
+        reverse(0, k-1) 
+        reverse(k, n-1)
