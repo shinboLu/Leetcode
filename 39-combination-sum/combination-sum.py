3f815ef26c05ca3407e062d4547cca4a33aa12e4
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-12 13:33:25 -0700

    Time: 15 ms (22.07%) | Memory: 19.6 MB (32.98%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -1,19 +1,18 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-
-        def bt(index, combs):
-            if sum(combs) == target:
-                res.append(combs.copy())
+        def dfs(cur_idx, cur_comb):
+            if sum(cur_comb) == target:
+                res.append(cur_comb.copy())
                 return
-
-            if sum(combs) > target:
+            if sum(cur_comb) > target:
                 return 
-            
-            for i in range(index, len(candidates)):
-                combs.append(candidates[i])
-                bt(i, combs)
-                combs.pop()
-            
-        bt(0, [])
-        return res 
+            for i in range(cur_idx, len(candidates)):
+                cur_comb.append(candidates[i]) 
+                dfs(i, cur_comb)
+                cur_comb.pop()
+        cur_comb = []
+        dfs(0, cur_comb)
+        return res
+
+
