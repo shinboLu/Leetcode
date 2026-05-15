3eae7505ac0e0d704e13e9682ff51bf3d110f27b
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-14 21:23:28 -0700

    Time: 15 ms (6.59%) | Memory: 19.2 MB (74.19%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.py
+++ 36-valid-sudoku/valid-sudoku.py
@@ -3,18 +3,20 @@ class Solution:
         row_counter = collections.defaultdict(list)
         col_counter = collections.defaultdict(list)
         matrix_counter = collections.defaultdict(lambda: collections.defaultdict(list))
+        nrow = len(board)
+        ncol = len(board[0])
 
-        for row in range(9):
-            for col in range(9):
-                cur_val = board[row][col]
-
-                if cur_val != '.':
-                    if cur_val in row_counter[row] or cur_val in col_counter[col] or cur_val in matrix_counter[row//3%3][col//3%3]:
-                        print(row_counter, col_counter, matrix_counter)
+        for row in range(nrow):
+            for col in range(ncol):
+                if board[row][col] != '.':
+                    if board[row][col] in row_counter[row]:
                         return False
-                
-                row_counter[row].append(cur_val)
-                col_counter[col].append(cur_val)
-                matrix_counter[row//3%3][col//3%3].append(cur_val)
-        
+                    elif board[row][col] in col_counter[col]:
+                        return False
+                    elif board[row][col] in matrix_counter[row//3%3][col//3%3]:
+                        return False
+                row_counter[row].append(board[row][col])
+                col_counter[col].append(board[row][col])
+                matrix_counter[row//3%3][col//3%3].append(board[row][col])
         return True
+
