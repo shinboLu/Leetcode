413b655e5e4fa758bebea4aac4571a5a87dd4e4c
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-16 22:09:35 -0700

    Time: 224 ms (14.99%) | Memory: 23.6 MB (5.18%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -3,22 +3,19 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        queue = collections.deque()
 
         nrow = len(matrix)
         ncol = len(matrix[0])
 
-        queue = collections.deque()
+        for r in range(nrow):
+            for c in range(ncol):
+                if matrix[r][c] == 0:
+                    queue.append([r, c])
 
-        for i in range(nrow):
-            for j in range(ncol):
-                if matrix[i][j] == 0:
-                    queue.append((i, j))
-
-        
         while queue:
-            cur_row, cur_col = queue.popleft()
-
+            cur_x, cur_y = queue.popleft()
             for i in range(nrow):
-                for j in range(ncol):
-                    if i == cur_row or j == cur_col:
-                        matrix[i][j] = 0
+                matrix[i][cur_y] = 0
+            for j in range(ncol):
+                matrix[cur_x][j] = 0
