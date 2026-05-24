10acce9b6170758eee1a60c19cf1bc300d575399
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-23 22:04:15 -0700

    Time: 226 ms (12.72%) | Memory: 26 MB (33.98%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.py
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -1,17 +1,16 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        mapping = collections.defaultdict(int)
-        res = 0
+        res = 0 
         left = 0 
+        mapping = collections.defaultdict(int)
 
         for right in range(len(fruits)):
-            mapping[fruits[right]] +=1
-
+            
+            mapping[fruits[right]] += 1
             while len(mapping) > 2:
                 mapping[fruits[left]] -=1
                 if mapping[fruits[left]] == 0:
                     del mapping[fruits[left]]
-                left +=1
+                left+=1
             res = max(res, sum(mapping.values()))
-
         return res
