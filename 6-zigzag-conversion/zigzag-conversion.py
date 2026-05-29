239c49dd9d3bb240cdcd9f6ec681209a582c05b5
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-29 16:28:12 -0700

    Time: 11 ms (56.04%) | Memory: 19.3 MB (46.73%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,23 +1,18 @@
 class Solution:
     def convert(self, s: str, num_rows: int) -> str:
-        if num_rows == 1 or num_rows >= len(s): 
+        if num_rows == 1 or num_rows >= len(s):
             return s
+        res = [''] * num_rows
+        cur_idx = 0 
         cur_dir = 1
-        row = [""] * num_rows
-        print(row)
-        cur_idx = 0
 
         for char in s:
-            row[cur_idx] += char
+            res[cur_idx] += char
 
-            if cur_idx ==0:
+            if cur_idx == 0:
                 cur_dir = 1
-            elif cur_idx == num_rows-1:
+            elif cur_idx >= num_rows-1:
                 cur_dir = -1
-            
-            cur_idx += cur_dir
-        return ''.join(row)
-            
 
- 
-            
+            cur_idx += cur_dir
+        return ''.join(res)
