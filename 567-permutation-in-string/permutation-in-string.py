f0916c0f069f7b934ea4554579e1ebc7885ad3b0
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-29 22:03:36 -0700

    Time: 27 ms (41.60%) | Memory: 19.4 MB (19.14%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -5,15 +5,16 @@ class Solution:
         left = 0 
 
         for right in range(len(s2)):
-            visited[s2[right]] +=1
+            visited[s2[right]] += 1
 
-            if right - left +1 > len(s1):
-                visited[s2[left]] -=1
+            if right - left+1 > len(s1):
+                visited[s2[left]] -=1 
                 if visited[s2[left]] == 0:
-                    del visited[s2[left]]
-                left +=1
+                    del visited[s2[left]] 
+                left+=1
             
-            if visited == target_dict:
+            if target_dict == visited:
                 return True
         return False
-            
+
+
