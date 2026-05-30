37565b688c48b474d2016514be27440dd6b065e7
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-29 21:36:46 -0700

    Time: 20 ms (51.68%) | Memory: 19.3 MB (53.43%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,19 +1,19 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         target_dict = collections.Counter(s1)
-        visited = collections.Counter(s2[:len(s1)])
-        if target_dict == visited:
-            return True
+        visited = collections.defaultdict(int)
+        left = 0 
 
-        for i in range(len(s1), len(s2)): 
-            visited[s2[i]] += 1
-            visited[s2[i-len(s1)]] -=1
-            if visited[s2[i-len(s1)]] == 0:
-                del visited[i-len(s1)]
+        for right in range(len(s2)):
+            visited[s2[right]] +=1
+
+            if right - left +1 > len(s1):
+                visited[s2[left]] -=1
+                if visited[s2[left]] == 0:
+                    del visited[s2[left]]
+                left +=1
             
-            if visited == target_dict: 
+            if visited == target_dict:
                 return True
-
         return False
-
             
