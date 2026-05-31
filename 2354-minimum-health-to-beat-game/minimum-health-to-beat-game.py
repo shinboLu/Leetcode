af37384880d21bb32da059bb16bc305cf5f8bcc7
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-05-31 09:22:47 -0700

    Time: 5 ms (58.82%) | Memory: 31.6 MB (29.41%) - LeetSync

--- 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.py
+++ 2354-minimum-health-to-beat-game/minimum-health-to-beat-game.py
@@ -1,8 +1,14 @@
 class Solution:
     def minimumHealth(self, damage: List[int], armor: int) -> int:
-        tot_dmg = sum(damage)
-        max_dmg = max(damage)
-        reduction = min(max_dmg, armor)
 
-        return tot_dmg - reduction+1
+        total_dmg = sum(damage)
+        if armor == 0:
+            return total_dmg+1
+        max_dmg = max(damage)
         
+        if max_dmg < armor: 
+            total_dmg -= max_dmg
+        else:
+            total_dmg -= armor
+            
+        return total_dmg+1
