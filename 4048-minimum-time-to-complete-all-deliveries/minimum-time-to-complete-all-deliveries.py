a08d449c29c9137c912860d609965370a2005ae2
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-06-06 20:35:08 -0700

    Time: 18 ms (29.67%) | Memory: 19.3 MB (60.44%) - LeetSync

--- 4048-minimum-time-to-complete-all-deliveries/minimum-time-to-complete-all-deliveries.py
+++ 4048-minimum-time-to-complete-all-deliveries/minimum-time-to-complete-all-deliveries.py
@@ -3,17 +3,10 @@ class Solution:
         left = 1
         right = 10 ** 18 
         multi = lcm(r[0], r[1])
-
         while left <= right:
-            ## mid is the total time in hours to complete all delivery
-            mid = (left+right) //2 
-
-            if d[0] <= mid - mid//r[0] and d[1] <= mid - mid//r[1] and d[0] + d[1] <= mid - mid//multi:
-                right = mid -1
+            mid = (left+right)//2
+            if d[0] <= mid - mid//r[0] and d[1] <= mid - mid//r[1] and d[0]+d[1] <= mid - mid // multi: 
+                right = mid-1
             else:
-                left = mid +1 
-
+                left = mid+1
         return left
-            
-
-        
