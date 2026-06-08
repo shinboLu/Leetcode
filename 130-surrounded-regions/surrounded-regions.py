6df75104644bbc36c25a0d6d2eff4bc3119d9952
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-06-07 20:12:48 -0700

    Time: 6 ms (58.57%) | Memory: 22.2 MB (91.55%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,35 +3,31 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        dirs = [[0,1],[1,0],[-1,0],[0,-1]]
         nrow = len(board)
         ncol = len(board[0])
+        dirs = [[0,1], [1,0], [0,-1], [-1, 0]]
 
-        def dfs(x, y):
-            if x<0 or x >= nrow or y < 0 or y >=ncol or board[x][y]!='O':
-                return
-            board[x][y] = 'V'
 
-            for dx, dy in dirs:
-                nx, ny = dx+x, dy+y
-                dfs(nx, ny)
-
+        queue = collections.deque()
         for row in range(nrow):
-            for col in [0, ncol-1]:
-                dfs(row, col)
+            for col in range(ncol):
+                if (row ==0 or col ==0 or row == nrow-1 or col == ncol-1) and board[row][col]=='O':
+                    queue.append([row, col])
 
-        for col in range(ncol):
-            for row in [0, nrow-1]:
-                dfs(row, col)
-        print(board)
-
+        while queue:
+            cx, cy = queue.popleft()
+            board[cx][cy] = 'V'
+            for dx, dy in dirs:
+                nx, ny = dx+cx, dy+cy
+                if 0<=nx<nrow and 0<=ny<ncol and board[nx][ny] == 'O':
+                    queue.append([nx, ny]) 
         for row in range(nrow):
             for col in range(ncol):
                 if board[row][col] == 'O':
                     board[row][col] = 'X'
-                elif board[row][col] =='V':
-                    board[row][col] = 'O'
+                elif board[row][col] == 'V':
+                    board[row][col]='O'
 
+        
 
-
-
+        
