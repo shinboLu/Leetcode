cabebbc38fb3fa9cc4bba922c3a6cc108e8e42a4
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-06-11 20:15:11 -0700

    Time: 311 ms (68.42%) | Memory: 31.6 MB (92.71%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -1,33 +1,31 @@
 class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        left = 0 
+        if m*k>len(bloomDay):
+            return -1
+        left = 1
         right = max(bloomDay)
 
-        def check(mid):
-            bouquets = 0 
-            cur_flower = 0 
-            for i in range(len(bloomDay)):
-                if bloomDay[i] <= mid:
-                    cur_flower += 1
+        def calc_bouquets(curday):
+            tot_bouquets = 0 
+            cur_flower_count = 0 
+            for day in bloomDay: 
+                if day <= curday: 
+                    cur_flower_count += 1
                 else:
-                    cur_flower = 0 
-                if cur_flower == k:
-                    bouquets +=1
-                    cur_flower = 0
-            return bouquets
-        res = -1
-        while left <= right:
-            mid = (left+right)//2 
-            if check(mid) >= m:
-                res = mid
-                right = mid -1
-            else:
-                left = mid + 1
-        return res
+                    cur_flower_count = 0
+                if cur_flower_count == k:
+                    tot_bouquets +=1
+                    cur_flower_count = 0
+                
+            return tot_bouquets
 
+        while left < right:
+            cur_day = (left+right)//2 
+            tot_bouquets = calc_bouquets(cur_day)
+            if tot_bouquets < m:
+                left = cur_day+1
+            else: 
+                right = cur_day
 
-
-
-
-
+        return left 
 
