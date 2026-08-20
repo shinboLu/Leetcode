9db8fc71be71c781da1535f199a92e9457dd1466
Xinbo Lu <123519065+shinboLu@users.noreply.github.com>
2026-08-19 21:56:45 -0700

    Time: 4 ms (14.72%) | Memory: 19.3 MB (63.48%) - LeetSync

--- 71-simplify-path/simplify-path.py
+++ 71-simplify-path/simplify-path.py
@@ -1,13 +1,14 @@
 class Solution:
     def simplifyPath(self, path: str) -> str:
-        stack = [] 
+        stack = []
         for subpath in path.split('/'):
             if subpath == '..':
                 if stack:
                     stack.pop()
-            elif subpath == '.' or not subpath:
+            elif not subpath or subpath == '.': 
                 continue
             else:
                 stack.append(subpath)
+
 
         return '/' + '/'.join(stack)
